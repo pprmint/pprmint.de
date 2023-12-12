7510e14b3ac4a67b90b2044a371a5a140785a448
pprmint <git@ppr.one>
2023-12-13 00:22:03 +0100

    Update privacy policy to reflect hosting changes.

--- pages/privacy.tsx
+++ pages/privacy.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from "next/router";
 import Link from "next/link";
 import useTranslation from "next-translate/useTranslation";
 import Trans from "next-translate/Trans";
@@ -7,45 +8,67 @@ import Title from "components/Title";
 
 export default function PrivacyPolicy() {
 	const { t } = useTranslation();
+	const { locale } = useRouter();
 	return (
 		<>
-			<Head
-				title={t("PRIVACY:Head.title")}
-				description={t("PRIVACY:Head.description")}
-			/>
-			<Title
-				title={t("PRIVACY:Head.title")}
-				description={t("PRIVACY:Head.description")}
-			/>
+			<Head title={t("PRIVACY:Head.title")} description={t("PRIVACY:Head.description")} />
+			<Title title={t("PRIVACY:Head.title")} description={t("PRIVACY:Head.description")} />
 			<main className="max-w-7xl mx-auto px-6 md:px-9">
 				<section className="my-12">
 					<div className="bg-neutral-900 rounded-xl p-6">
 						<h2 className="font-display font-semibold text-neutral-50 text-3xl md:text-4xl pb-3">
 							{t("PRIVACY:Content.Tldr.heading")}
 						</h2>
-						<p>{t("PRIVACY:Content.Tldr.text")}</p>
+						<p>
+							<Trans
+								i18nKey="PRIVACY:Content.Tldr.text1"
+								components={{
+									b: <b className="text-neutral-50" />,
+								}}
+							/>
+						</p>
+						<p>
+							<Trans
+								i18nKey="PRIVACY:Content.Tldr.text2"
+								components={{
+									b: <b className="text-neutral-50" />,
+								}}
+							/>
+						</p>
+						<p>
+							<Trans
+								i18nKey="PRIVACY:Content.Tldr.text3"
+								components={{
+									b: <b className="text-neutral-50" />,
+								}}
+							/>
+						</p>
 					</div>
 				</section>
 				<section className="my-12">
 					<h2 className="font-display font-semibold text-neutral-50 text-3xl md:text-4xl pb-3">
 						{t("PRIVACY:Content.General.heading")}
 					</h2>
-					<p>
-						{t("PRIVACY:Content.General.text1")}
-						<br />
-						{t("PRIVACY:Content.General.text2")}
-						<br />
+					<p>{t("PRIVACY:Content.General.text1")}</p>
+					<p>{t("PRIVACY:Content.General.text2")}</p>
+					<p>{t("PRIVACY:Content.General.text3")}</p>
+                    <p className="flex gap-6 flex-wrap">
 						<Link
-							href=""
-							className="font-medium text-blue underline decoration-dotted hover:decoration-solid decoration-blue-800 hover:decoration-blue duration-100"
-							scroll={false}
+							className="font-medium text-blue underline decoration-dotted decoration-2 decoration-blue-800 hover:decoration-blue"
+							href="https://www.fastmail.com/about/privacy/"
+							target="_blank"
+							rel="noopener noreferrer"
 						>
-							<Trans
-								i18nKey="PRIVACY:Content.General.example"
-								components={{
-									strong: <strong />,
-								}}
-							/>
+							{t("PRIVACY:Content.privacyPolicyOf", { provider: "Fastmail" })}
+							<i className="ri-arrow-right-up-line" />
+						</Link>
+						<Link
+							className="font-medium text-blue underline decoration-dotted decoration-2 decoration-blue-800 hover:decoration-blue"
+							href="https://www.fastmail.com/about/dpa/"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							{t("PRIVACY:Content.dpaOf", { provider: "Fastmail" })}
 							<i className="ri-arrow-right-up-line" />
 						</Link>
 					</p>
@@ -56,141 +79,45 @@ export default function PrivacyPolicy() {
 						{t("PRIVACY:Content.Hosting.heading")}
 					</h2>
 					<p>{t("PRIVACY:Content.Hosting.text1")}</p>
-					<p>
-						<Trans
-							i18nKey="PRIVACY:Content.Hosting.text2"
-							components={{
-								strong: <strong />,
-							}}
-						/>
-					</p>
-					<p>
-						<Trans
-							i18nKey="PRIVACY:Content.Hosting.text3"
-							components={{
-								strong: <strong />,
-							}}
-						/>
-					</p>
-					<p>{t("PRIVACY:Content.Hosting.text4")}</p>
-					<div className="pl-6 pb-4">
-						<p>
-							<Trans
-								i18nKey="PRIVACY:Content.Hosting.Log.domain"
-								components={{
-									strong: <strong />,
-								}}
-							/>
-						</p>
-						<p>
-							<Trans
-								i18nKey="PRIVACY:Content.Hosting.Log.ipAddress"
-								components={{
-									strong: <strong />,
-								}}
-							/>
-						</p>
-						<p>
-							<Trans
-								i18nKey="PRIVACY:Content.Hosting.Log.accessTime"
-								components={{
-									strong: <strong />,
-								}}
-							/>
-						</p>
-						<p>
-							<Trans
-								i18nKey="PRIVACY:Content.Hosting.Log.receivedFile"
-								components={{
-									strong: <strong />,
-								}}
-							/>
-						</p>
-						<p>
-							<Trans
-								i18nKey="PRIVACY:Content.Hosting.Log.httpCode"
-								components={{
-									strong: <strong />,
-								}}
-							/>
-							<br />
-							<Link
-								className="font-medium text-blue underline decoration-dotted decoration-2 decoration-blue-800 hover:decoration-blue"
-								href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Status"
-								target="_blank"
-								rel="noopener noreferrer"
-							>
-								HTTP response status codes - HTTP | MDN
-								<i className="ri-arrow-right-up-line" />
-							</Link>
-						</p>
-						<p>
-							<Trans
-								i18nKey="PRIVACY:Content.Hosting.Log.userAgent"
-								components={{
-									strong: <strong />,
-								}}
-							/>
-							<br />
-							<Link
-								className="font-medium text-blue underline decoration-dotted decoration-2 decoration-blue-800 hover:decoration-blue"
-								href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/User-Agent"
-								target="_blank"
-								rel="noopener noreferrer"
-							>
-								User-Agent - HTTP | MDN
-								<i className="ri-arrow-right-up-line" />
-							</Link>
-						</p>
-					</div>
-					<p>
-						{t("PRIVACY:Content.Hosting.text5")}
-						<br />
-					</p>
-					<p>
-						{t("PRIVACY:Content.Hosting.text6")}
-						<br />
-					</p>
-					<p>
+					<p>{t("PRIVACY:Content.Hosting.text2")}</p>
+					<p>{t("PRIVACY:Content.Hosting.text3")}</p>
+					<p className="flex gap-6 flex-wrap">
 						<Link
 							className="font-medium text-blue underline decoration-dotted decoration-2 decoration-blue-800 hover:decoration-blue"
-							href="https://vercel.com/legal/privacy-policy"
+                            href={`https://contabo.com/${locale}/legal/privacy/`}
 							target="_blank"
 							rel="noopener noreferrer"
 						>
 							{t("PRIVACY:Content.privacyPolicyOf", {
-								provider: "Vercel",
+                                provider: "Contabo",
 							})}
 							<i className="ri-arrow-right-up-line" />
 						</Link>
-					</p>
-					<p>
 						<Link
 							className="font-medium text-blue underline decoration-dotted decoration-2 decoration-blue-800 hover:decoration-blue"
-							href="https://vercel.com/legal/dpa"
+                            href="https://www.cloudflare.com/privacypolicy/"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							{t("PRIVACY:Content.privacyPolicyOf", {
+								provider: "Cloudflare",
+							})}
+							<i className="ri-arrow-right-up-line" />
+						</Link>
+						<Link
+							className="font-medium text-blue underline decoration-dotted decoration-2 decoration-blue-800 hover:decoration-blue"
+                            href="https://www.cloudflare.com/cloudflare-customer-dpa/"
 							target="_blank"
 							rel="noopener noreferrer"
 						>
 							{t("PRIVACY:Content.dpaOf", {
-								provider: "Vercel",
-							})}
-							<i className="ri-arrow-right-up-line" />
-						</Link>
-					</p>
-					<p>
-						<Link
-							className="font-medium text-blue underline decoration-dotted decoration-2 decoration-blue-800 hover:decoration-blue"
-							href="https://www.hetzner.com/legal/privacy-policy"
-							target="_blank"
-							rel="noopener noreferrer"
-						>
-							{t("PRIVACY:Content.privacyPolicyOf", {
-								provider: "Hetzner",
+								provider: "Cloudflare",
 							})}
 							<i className="ri-arrow-right-up-line" />
 						</Link>
 					</p>
 				</section>
+
 				<section className="my-12">
 					<h2 className="font-display font-semibold text-neutral-50 text-3xl md:text-4xl pb-3">
 						{t("PRIVACY:Content.Contact.heading")}
@@ -211,17 +138,6 @@ export default function PrivacyPolicy() {
 								strong: <strong />,
 							}}
 						/>
-					</p>
-					<p>
-						<Link
-							className="font-medium text-blue underline decoration-dotted decoration-2 decoration-blue-800 hover:decoration-blue"
-							href="https://www.fastmail.com/about/privacy/"
-							target="_blank"
-							rel="noopener noreferrer"
-						>
-							{t("PRIVACY:Content.privacyPolicyOf", { provider: "Fastmail" })}
-							<i className="ri-arrow-right-up-line" />
-						</Link>
 					</p>
 				</section>
 			</main>
