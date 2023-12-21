615358cb08dacfd4e8dad82abdf792c69efef255
pprmint <git@ppr.one>
2023-12-21 21:28:56 +0100

    Update packages and finally fix this fucking god damn title alignment because Apple is so fucking horny over adding blur behind shit that on shitty fucking Safari on iOS the bottom bar would cover parts of elements that were 100vh tall so now we had to include new fucking units just to accomodate for shitty things like this. I hate WebKit too. Fuck you.

--- components/Title.tsx
+++ components/Title.tsx
@@ -28,16 +28,16 @@ export default function Title(
 		delay: 250,
 	});
 	return (
-		<div className="relative w-screen h-screen overflow-hidden">
-			<a.div className="absolute w-screen h-screen" style={{ ...fadeIn }}>
-				<div className="absolute w-screen h-screen">{props.children}</div>
+		<div className="relative w-screen h-svh overflow-hidden">
+			<a.div className="absolute w-full h-full" style={{ ...fadeIn }}>
+				<div className="absolute w-full h-full">{props.children}</div>
 			</a.div>
-			<div className="absolute w-screen h-screen bg-gradient-to-t from-neutral-950" />
+			<div className="absolute w-full h-full bg-gradient-to-t from-neutral-950 via-transparent" />
 			<a.div
-				style={{ ...inFromBottom }}
-				className="absolute flex w-screen h-screen p-6 md:p-12 flex-col md:flex-row"
+				className="absolute bottom-0 flex w-full gap-3 p-6 md:p-12 items-end"
+                style={inFromBottom}
 			>
-				<div className="my-auto md:mt-auto md:mb-0 md:mr-auto">
+				<div className="flex-grow">
 					<h1 className="font-display font-semibold text-neutral-50 text-4xl md:text-5xl lg:text-6xl xl:text-8xl pb-1 md:pb-3">
 						{props.title}
 						<span className={accentColor}>.</span>
@@ -45,7 +45,7 @@ export default function Title(
 					<h2 className="text-neutral text-xl md:text-2xl xl:text-3xl">{props.description}</h2>
 				</div>
 				<i
-					className={`ri-arrow-down-line text-3xl ${accentColor} self-center md:self-end animate-arrow-fade-down opacity-0`}
+					className={`ri-arrow-down-line text-3xl ${accentColor} animate-arrow-fade-down opacity-0`}
 				/>
 			</a.div>
 		</div>
