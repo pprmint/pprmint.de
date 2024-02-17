8842166565624fbea3a3e36d8f8735db322b1d39
pprmint <git@pprmint.art>
2024-02-17 21:35:45 +0100

    Remove react-parallax-mouse.

--- pages/404.tsx
+++ pages/404.tsx
@@ -3,7 +3,6 @@ import Image from "next/image";
 import useTranslation from "next-translate/useTranslation";
 import { useSpring, a, config, easings } from "@react-spring/web";
 import Trans from "next-translate/Trans";
-import { MouseParallaxChild, MouseParallaxContainer } from "react-parallax-mouse";
 
 import Head from "components/Head";
 import Button from "components/Button";
@@ -25,11 +24,11 @@ export default function NotFound() {
 			clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
 			opacity: 1,
 		},
-        delay: 1650,
-        config: {
-            easing: easings.easeOutExpo,
-            duration: 1000,
-        }
+		delay: 1650,
+		config: {
+			easing: easings.easeOutExpo,
+			duration: 1000,
+		},
 	});
 	const fadeIn = useSpring({
 		from: { opacity: 0 },
@@ -56,42 +55,28 @@ export default function NotFound() {
 	return (
 		<>
 			<Head title={t("404:Head.title")} description={t("404:Content.info")} color="#ff7711" />
-			<MouseParallaxContainer
-				containerStyle={{ width: "100vw", height: "100vh", minHeight: "600px" }}
-				springConfig={config.gentle}
-				resetOnLeave
-			>
-				<MouseParallaxChild
-					factorX={-0.05}
-					factorY={-0.05}
-					style={{
-						display: "flex",
-						alignItems: "center",
-						position: "absolute",
-						width: "100%",
-						height: "100vh",
-					}}
-				>
+			<main className="relative w-screen h-screen overflow-clip">
+				<div className="absolute inset-0">
 					<a.div
 						className="text-orange-800 font-display-mono font-light blur-sm lg:blur-lg w-full text-center -skew-y-6"
 						style={{ fontSize: "50vw", ...fadeIn }}
 					>
 						{countUp.val.to((val) => Math.floor(val))}
 					</a.div>
-				</MouseParallaxChild>
-				<div className="absolute flex items-center justify-center top-0 left-0 w-screen h-screen">
-					<a.div
-						style={{ ...ripple }}
-						className="bg-orange-800 aspect-square h-screen md:h-auto md:w-screen rounded-full"
+					<div className="absolute flex items-center justify-center top-0 left-0 w-screen h-screen">
+						<a.div
+							style={{ ...ripple }}
+							className="bg-orange-800 aspect-square h-screen md:h-auto md:w-screen rounded-full"
+						/>
+					</div>
+					<div
+						className="absolute w-full h-full top-0 left-0"
+						style={{
+							backgroundImage: `url('data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" xml:space="preserve" width="24" height="24" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2"><path d="M0 10V0h10a2 2 0 0 0 4 0h10v10a2 2 0 0 0 0 4v10H14a2 2 0 0 0-4 0H0V14a2 2 0 0 0 0-4Z" style="fill:%23111"/></svg>')`,
+							backgroundRepeat: "repeat",
+						}}
 					/>
 				</div>
-				<div
-					className="absolute w-full h-full top-0 left-0"
-					style={{
-						backgroundImage: `url('data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" xml:space="preserve" width="24" height="24" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2"><path d="M0 10V0h10a2 2 0 0 0 4 0h10v10a2 2 0 0 0 0 4v10H14a2 2 0 0 0-4 0H0V14a2 2 0 0 0 0-4Z" style="fill:%23111"/></svg>')`,
-						backgroundRepeat: "repeat",
-					}}
-				/>
 				<a.div
 					className="z-10 flex flex-col md:flex-row-reverse justify-center items-center gap-3 md:gap-6 w-full max-w-7xl h-full mx-auto px-6 md:px-9"
 					style={{ ...inFromBottom }}
@@ -119,7 +104,7 @@ export default function NotFound() {
 						/>
 					</p>
 				</a.div>
-			</MouseParallaxContainer>
+			</main>
 		</>
 	);
 }
