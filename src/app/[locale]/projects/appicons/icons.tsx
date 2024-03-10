e3f70dbd63fa5d9fec13f047ba6fa9c1e50c060e
pprmint <git@pprmint.art>
2024-03-10 19:01:18 +0100

    Add app icons page.

---
+++ src/app/[locale]/projects/appicons/icons.tsx
@@ -0,0 +1,375 @@
+import AcrobatIcon from "/public/assets/appicons/Acrobat.png";
+import AeroIcon from "/public/assets/appicons/Aero.png";
+import AfterEffectsIcon from "/public/assets/appicons/AfterEffects.png";
+import AnimateIcon from "/public/assets/appicons/Animate.png";
+import AuditionIcon from "/public/assets/appicons/Audition.png";
+import CharacterAnimatorIcon from "/public/assets/appicons/CharacterAnimator.png";
+import CreativeCloudClientIcon from "/public/assets/appicons/CreativeCloudClient.png";
+import DreamweaverIcon from "/public/assets/appicons/Dreamweaver.png";
+import FrescoIcon from "/public/assets/appicons/Fresco.png";
+import IllustratorIcon from "/public/assets/appicons/Illustrator.png";
+import InCopyIcon from "/public/assets/appicons/InCopy.png";
+import InDesignIcon from "/public/assets/appicons/InDesign.png";
+import LightroomIcon from "/public/assets/appicons/Lightroom.png";
+import LightroomClassicIcon from "/public/assets/appicons/LightroomClassic.png";
+import MediaEncoderIcon from "/public/assets/appicons/MediaEncoder.png";
+import MediaEncoderAltIcon from "/public/assets/appicons/MediaEncoderAlt.png";
+import PhotoshopIcon from "/public/assets/appicons/Photoshop.png";
+import PremiereProIcon from "/public/assets/appicons/PremierePro.png";
+import ScanIcon from "/public/assets/appicons/Scan.png";
+import XDIcon from "/public/assets/appicons/XD.png";
+import AffinityDesignerIcon from "/public/assets/appicons/AffinityDesigner.png";
+import AffinityPhotoIcon from "/public/assets/appicons/AffinityPhoto.png";
+import AffinityPublisherIcon from "/public/assets/appicons/AffinityPublisher.png";
+import AffinityDesignerAltIcon from "/public/assets/appicons/AffinityDesignerAlt.png";
+import AffinityPhotoAltIcon from "/public/assets/appicons/AffinityPhotoAlt.png";
+import AffinityPublisherAltIcon from "/public/assets/appicons/AffinityPublisherAlt.png";
+import AsepriteIcon from "/public/assets/appicons/Aseprite.png";
+import AsepriteAltIcon from "/public/assets/appicons/AsepriteAlt.png";
+import BlenderIcon from "/public/assets/appicons/Blender.png";
+import Cinema4DIcon from "/public/assets/appicons/Cinema4D.png";
+import ClipStudioIcon from "/public/assets/appicons/ClipStudio.png";
+import ClipStudioPaintIcon from "/public/assets/appicons/ClipStudioPaint.png";
+import Live2DCubismIcon from "/public/assets/appicons/Live2DCubism.png";
+import GraphicsGaleIcon from "/public/assets/appicons/GraphicsGale.png";
+import AbletonLiveIntroIcon from "/public/assets/appicons/AbletonLiveIntro.png";
+import AbletonLiveIntroAltIcon from "/public/assets/appicons/AbletonLiveIntroAlt.png";
+import AbletonLiveStandardIcon from "/public/assets/appicons/AbletonLiveStandard.png";
+import AbletonLiveStandardAltIcon from "/public/assets/appicons/AbletonLiveStandardAlt.png";
+import AbletonLiveSuiteIcon from "/public/assets/appicons/AbletonLiveSuite.png";
+import AbletonLiveSuiteAltIcon from "/public/assets/appicons/AbletonLiveSuiteAlt.png";
+import AbletonLiveLiteIcon from "/public/assets/appicons/AbletonLiveLite.png";
+import AbletonLiveLiteAltIcon from "/public/assets/appicons/AbletonLiveLiteAlt.png";
+import AbletonLiveBetaIcon from "/public/assets/appicons/AbletonLiveBeta.png";
+import AbletonLiveBetaAltIcon from "/public/assets/appicons/AbletonLiveBetaAlt.png";
+import FLStudioIcon from "/public/assets/appicons/FLStudio.png";
+import ReaperIcon from "/public/assets/appicons/Reaper.png";
+import LeagueIcon from "/public/assets/appicons/League.png";
+import MinecraftIcon from "/public/assets/appicons/Minecraft.png";
+import PrismLauncherIcon from "/public/assets/appicons/PrismLauncher.png";
+import SteamIcon from "/public/assets/appicons/Steam.png";
+import DiscordIcon from "/public/assets/appicons/Discord.png";
+import AegisubIcon from "/public/assets/appicons/Aegisub.png";
+import AudacityIcon from "/public/assets/appicons/Audacity.png";
+import DaVinciResolveIcon from "/public/assets/appicons/DaVinciResolve.png";
+import FileZillaIcon from "/public/assets/appicons/FileZilla.png";
+import GimpIcon from "/public/assets/appicons/GIMP.png";
+import KritaIcon from "/public/assets/appicons/Krita.png";
+import OBSIcon from "/public/assets/appicons/OBS.png";
+import OBSAltIcon from "/public/assets/appicons/OBSAlt.png";
+import qBittorrentIcon from "/public/assets/appicons/qBittorrent.png";
+import SpotifyIcon from "/public/assets/appicons/Spotify.png";
+import TixatiIcon from "/public/assets/appicons/Tixati.png";
+
+export const Icons = [
+	{
+		name: "Adobe Creative Cloud Client",
+		image: CreativeCloudClientIcon,
+		link: "/Adobe/Creative_Cloud_Client.ico",
+	},
+	{
+		name: "Adobe Acrobat",
+		image: AcrobatIcon,
+		link: "/Adobe/Acrobat.ico",
+	},
+	{
+		name: "Adobe Scan",
+		image: ScanIcon,
+		link: "/Adobe/Scan.ico",
+	},
+	{
+		name: "Adobe Aero",
+		image: AeroIcon,
+		link: "/Adobe/Aero.ico",
+	},
+	{
+		name: "Adobe Animate",
+		image: AnimateIcon,
+		link: "/Adobe/Animate.ico",
+	},
+	{
+		name: "Adobe After Effects",
+		image: AfterEffectsIcon,
+		link: "/Adobe/After_Effects.ico",
+	},
+	{
+		name: "Adobe Audition",
+		image: AuditionIcon,
+		link: "/Adobe/Audition.ico",
+	},
+	{
+		name: "Adobe Character Animator",
+		image: CharacterAnimatorIcon,
+		link: "/Adobe/Character_Animator.ico",
+	},
+	{
+		name: "Adobe Dreamweaver",
+		image: DreamweaverIcon,
+		link: "/Adobe/Dreamweaver.ico",
+	},
+	{
+		name: "Adobe Fresco",
+		image: FrescoIcon,
+		link: "/Adobe/Fresco.ico",
+	},
+	{
+		name: "Adobe Illustrator",
+		image: IllustratorIcon,
+		link: "/Adobe/Illustrator.ico",
+	},
+	{
+		name: "Adobe InCopy",
+		image: InCopyIcon,
+		link: "/Adobe/InCopy.ico",
+	},
+	{
+		name: "Adobe InDesign",
+		image: InDesignIcon,
+		link: "/Adobe/InDesign.ico",
+	},
+	{
+		name: "Adobe Lightroom",
+		image: LightroomIcon,
+		link: "/Adobe/Lightroom.ico",
+	},
+	{
+		name: "Adobe Lightroom Classic",
+		image: LightroomClassicIcon,
+		link: "/Adobe/Lightroom_Classic.ico",
+	},
+	{
+		name: "Adobe Media Encoder",
+		image: MediaEncoderIcon,
+		link: "/Adobe/Media_Encoder.ico",
+	},
+	{
+		name: "Adobe Media Encoder (Alt)",
+		image: MediaEncoderAltIcon,
+		link: "/Adobe/Media_Encoder_(alt).ico",
+	},
+	{
+		name: "Adobe Photoshop",
+		image: PhotoshopIcon,
+		link: "/Adobe/Photoshop.ico",
+	},
+	{
+		name: "Adobe Premiere Pro",
+		image: PremiereProIcon,
+		link: "/Adobe/Premiere_Pro.ico",
+	},
+	{
+		name: "Adobe XD",
+		image: XDIcon,
+		link: "/Adobe/XD.ico",
+	},
+	{
+		name: "Affinity Designer",
+		image: AffinityDesignerIcon,
+		link: "/Affinity/Designer.ico",
+	},
+	{
+		name: "Affinity Photo",
+		image: AffinityPhotoIcon,
+		link: "/Affinity/Photo.ico",
+	},
+	{
+		name: "Affinity Publisher",
+		image: AffinityPublisherIcon,
+		link: "/Affinity/Publisher.ico",
+	},
+	{
+		name: "Affinity Designer (alt)",
+		image: AffinityDesignerAltIcon,
+		link: "/Affinity/Designer_(alt).ico",
+	},
+	{
+		name: "Affinity Photo (alt)",
+		image: AffinityPhotoAltIcon,
+		link: "/Affinity/Photo_(alt).ico",
+	},
+	{
+		name: "Affinity Publisher (alt)",
+		image: AffinityPublisherAltIcon,
+		link: "/Affinity/Publisher_(alt).ico",
+	},
+	{
+		name: "Aseprite",
+		image: AsepriteIcon,
+		link: "/Art/Aseprite.ico",
+	},
+	{
+		name: "Aseprite (alt)",
+		image: AsepriteAltIcon,
+		link: "/Art/Aseprite_(alt).ico",
+	},
+	{
+		name: "Blender",
+		image: BlenderIcon,
+		link: "/Art/Blender.ico",
+	},
+	{
+		name: "Cinema 4D",
+		image: Cinema4DIcon,
+		link: "/Art/Cinema_4D.ico",
+	},
+	{
+		name: "Clip Studio",
+		image: ClipStudioIcon,
+		link: "/Art/Clip_Studio.ico",
+	},
+	{
+		name: "Clip Studio Paint",
+		image: ClipStudioPaintIcon,
+		link: "/Art/Clip_Studio_Paint.ico",
+	},
+	{
+		name: "GIMP",
+		image: GimpIcon,
+		link: "/Art/GIMP.ico",
+	},
+	{
+		name: "GraphicsGale",
+		image: GraphicsGaleIcon,
+		link: "/Art/GraphicsGale.ico",
+	},
+	{
+		name: "Krita",
+		image: KritaIcon,
+		link: "/Art/Krita.ico",
+	},
+	{
+		name: "Live2D Cubism",
+		image: Live2DCubismIcon,
+		link: "/Art/Live2D_Cubism.ico",
+	},
+	{
+		name: "League of Legends",
+		image: LeagueIcon,
+		link: "/Games/League.ico",
+	},
+	{
+		name: "Minecraft",
+		image: MinecraftIcon,
+		link: "/Games/Minecraft_grass.ico",
+	},
+	{
+		name: "Prism Launcher",
+		image: PrismLauncherIcon,
+		link: "/Games/Prism_Launcher.ico",
+	},
+	{
+		name: "Steam",
+		image: SteamIcon,
+		link: "/Games/Steam.ico",
+	},
+	{
+		name: "Discord",
+		image: DiscordIcon,
+		link: "/Chat/Discord.ico",
+	},
+	{
+		name: "FL Studio",
+		image: FLStudioIcon,
+		link: "/DAWs/FL_Studio.ico",
+	},
+	{
+		name: "Reaper",
+		image: ReaperIcon,
+		link: "/DAWs/Reaper.ico",
+	},
+	{
+		name: "Ableton Live Intro",
+		image: AbletonLiveIntroIcon,
+		link: "/DAWs/Ableton_Live_Intro.ico",
+	},
+	{
+		name: "Ableton Live Intro (alt)",
+		image: AbletonLiveIntroAltIcon,
+		link: "/DAWs/Ableton_Live_Intro_(alt).ico",
+	},
+	{
+		name: "Ableton Live Standard",
+		image: AbletonLiveStandardIcon,
+		link: "/DAWs/Ableton_Live_Standard.ico",
+	},
+	{
+		name: "Ableton Live Standard (alt)",
+		image: AbletonLiveStandardAltIcon,
+		link: "/DAWs/Ableton_Live_Standard_(alt).ico",
+	},
+	{
+		name: "Ableton Live Suite",
+		image: AbletonLiveSuiteIcon,
+		link: "/DAWs/Ableton_Live_Suite.ico",
+	},
+	{
+		name: "Ableton Live Suite (alt)",
+		image: AbletonLiveSuiteAltIcon,
+		link: "/DAWs/Ableton_Live_Suite_(alt).ico",
+	},
+	{
+		name: "Ableton Live Lite",
+		image: AbletonLiveLiteIcon,
+		link: "/DAWs/Ableton_Live_Lite.ico",
+	},
+	{
+		name: "Ableton Live Lite (alt)",
+		image: AbletonLiveLiteAltIcon,
+		link: "/DAWs/Ableton_Live_Lite_(alt).ico",
+	},
+	{
+		name: "Ableton Live Beta",
+		image: AbletonLiveBetaIcon,
+		link: "/DAWs/Ableton_Live_Beta.ico",
+	},
+	{
+		name: "Ableton Live Beta (alt)",
+		image: AbletonLiveBetaAltIcon,
+		link: "/DAWs/Ableton_Live_Beta_(alt).ico",
+	},
+	{
+		name: "Aegisub",
+		image: AegisubIcon,
+		link: "/Various/Aegisub.ico",
+	},
+	{
+		name: "Audacity",
+		image: AudacityIcon,
+		link: "/Various/Audacity.ico",
+	},
+	{
+		name: "DaVinci Resolve",
+		image: DaVinciResolveIcon,
+		link: "/Various/Resolve.ico",
+	},
+	{
+		name: "FileZilla",
+		image: FileZillaIcon,
+		link: "/Various/FileZilla.ico",
+	},
+	{
+		name: "OBS",
+		image: OBSIcon,
+		link: "/Various/OBS.ico",
+	},
+	{
+		name: "OBS (Alt)",
+		image: OBSAltIcon,
+		link: "/Various/OBS_(alt).ico",
+	},
+	{
+		name: "qBittorrent",
+		image: qBittorrentIcon,
+		link: "/Various/qBittorrent.ico",
+	},
+	{
+		name: "Spotify",
+		image: SpotifyIcon,
+		link: "/Various/Spotify.ico",
+	},
+	{
+		name: "Tixati",
+		image: TixatiIcon,
+		link: "/Various/Tixati.ico",
+	},
+];
