e64242da2ce6c213380840791517a8f1efbac52b
pprmint <git@pprmint.art>
2024-03-11 19:53:21 +0100

    Now it's back fr.

--- src/app/[locale]/mina/filters.tsx
+++ src/app/[locale]/mina/filters.tsx
@@ -7,24 +7,34 @@ import { Filter } from "lucide-react";
 import { easings, useTransition, a } from "@react-spring/web";
 import Checkbox from "src/components/ui/Checkbox";
 import { Dialog } from "@radix-ui/react-dialog";
+import { useTranslations } from "next-intl";
+import dynamic from "next/dynamic";
 
-export default function Filters(props: { nsfw?: string; artist?: string; searchParams?: string }) {
+function Filters(props: { nsfw?: string; artist?: string }) {
+	const t = useTranslations("MINA");
 	const searchParams = useSearchParams();
 	const nsfw = props.nsfw == "show" ? true : false;
 	const [visible, setVisible] = useState(false);
 	const pathname = usePathname();
 	const { replace } = useRouter();
 	const seenNsfwDialog = localStorage.getItem("confirmedNsfwDialog");
+	function handleDialogAccept() {
+		const params = new URLSearchParams(searchParams);
+		localStorage.setItem("confirmedNsfwDialog", "interestingly, yes");
+		params.set("nsfw", "show");
+		setDialogOpen(false);
+		replace(`${pathname}?${params.toString()}`, { scroll: false });
+	}
 	function handleNsfw() {
 		const params = new URLSearchParams(searchParams);
 		if (nsfw) {
 			params.delete("nsfw");
 		} else {
 			if (!seenNsfwDialog) {
-				// setDialogOpen(true);
-                console.log("Later.")
+				setDialogOpen(true);
 			} else {
 				params.set("nsfw", "show");
+				setDialogOpen(false);
 			}
 		}
 		replace(`${pathname}?${params.toString()}`, { scroll: false });
@@ -36,17 +46,25 @@ export default function Filters(props: { nsfw?: string; artist?: string; searchP
 		from: { opacity: 0 },
 		enter: {
 			opacity: 1,
+			config: {
+				easing: easings.easeOutCubic,
+				duration: 200,
+			},
 		},
 		leave: {
 			opacity: 0,
+			config: {
+				easing: easings.easeOutCubic,
+				duration: 200,
+			},
 		},
 	});
 
 	return (
 		<>
-			<div className="flex gap-3 items-center mb-3 w-fit bg-transparent border border-neutral-900 rounded-full">
+			<div className="flex gap-3 items-center mb-3 w-fit bg-transparent border border-neutral-900 rounded-lg overflow-hidden">
 				<button
-					className={`h-10 px-4 rounded-full text-neutral-50 bg-neutral-900 hover:bg-neutral-800 active:bg-neutral-900 disabled:text-neutral-800 disabled:bg-transparent duration-100`}
+					className={`h-10 px-4 rounded-r-lg text-neutral-50 bg-neutral-900 hover:bg-neutral-800 active:bg-neutral-900 disabled:text-neutral-800 disabled:bg-transparent duration-100`}
 					onClick={() => setVisible(!visible)}
 				>
 					<span className="flex gap-3 items-center">
@@ -57,15 +75,15 @@ export default function Filters(props: { nsfw?: string; artist?: string; searchP
 				{visible && (
 					<div className="flex gap-3 items-center mr-3">
 						<Checkbox id="nsfw" checked={nsfw} onCheckedChange={handleNsfw} />
-						<label htmlFor="nsfw">Show NSFW</label>
+						<label htmlFor="nsfw">{t("Content.NSFW.checkbox")}</label>
 					</div>
 				)}
 			</div>
 			<Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
 				{dialogTransitions((styles, item) =>
 					item ? (
-						<a.div style={styles}>
-							<NsfwDialog />
+						<a.div style={styles} className="fixed inset-0 z-100">
+							<NsfwDialog onAccept={handleDialogAccept} />
 						</a.div>
 					) : null
 				)}
@@ -73,3 +91,7 @@ export default function Filters(props: { nsfw?: string; artist?: string; searchP
 		</>
 	);
 }
+
+export default dynamic(() => Promise.resolve(Filters), {
+	ssr: false,
+});
--- src/app/[locale]/mina/nsfwDialog.tsx
+++ src/app/[locale]/mina/nsfwDialog.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { useState, useEffect } from "react";
 import Image from "next/image";
-import { useSearchParams } from "next/navigation";
 import { useTranslations } from "next-intl";
 import { useSpring, useTransition, easings, a } from "@react-spring/web";
 import * as Dialog from "@radix-ui/react-dialog";
@@ -10,11 +9,11 @@ import Button from "src/components/ui/Button";
 
 import HehMina from "public/assets/mina/heh-upscaled.png";
 import { Link } from "src/navigation";
+import FadingImage from "src/components/ui/FadingImage";
+import { X } from "lucide-react";
 
-export default function NsfwDialog() {
+export default function NsfwDialog({ onAccept }: { onAccept: () => void }) {
 	const t = useTranslations("MINA");
-	const searchParams = useSearchParams();
-	const params = new URLSearchParams(searchParams);
 
 	const [timer, setTimer] = useState(0);
 	useEffect(() => {
@@ -36,80 +35,68 @@ export default function NsfwDialog() {
 	});
 
 	return (
-		<>
-			<Dialog.Overlay className="fixed inset-0 z-60 bg-neutral-950/90 backdrop-blur-lg data-[state=open]:animate-fade-in data-[state=closed]:animate-fade-out" />
+		<Dialog.Portal>
+			<Dialog.Overlay className="bg-neutral-950/90 data-[state=open]:animate-fade-in data-[state=closed]:animate-fade-out fixed inset-0 z-50" />
 			<Dialog.Content
-				forceMount
-				asChild
-				className="fixed z-60 inset-0 flex flex-col items-center md:justify-center px-6 py-9 max-h-screen overflow-auto data-[state=open]:animate-scale-up"
+				className={`fixed inset-0 lg:inset-auto overflow-auto z-[9999] flex flex-col lg:flex-row gap-12 items-center lg:top-1/2 lg:left-1/2 w-screen max-w-5xl lg:-translate-x-1/2 lg:-translate-y-1/2 p-6 md:p-9 bg-neutral-950 lg:border border-neutral-900 ring-1 ring-neutral-950 lg:shadow-xl shadow-neutral-950/50 lg:rounded-xl data-[state=open]:animate-scale-up data-[state=closed]:animate-scale-down focus:outline-none origin-center lg:origin-top-left`}
 			>
-				<div className="animate-scale-up">
-					<Dialog.Close className="group fixed z-50 top-3 md:top-5 right-3 md:right-5 text-neutral-50 w-10 h-10 rounded-full bg-neutral-50/10 hover:bg-neutral-50/20 duration-100 text-xl">
-						<i className="ri-close-line text-xl text-neutral-50" />
-					</Dialog.Close>
-					<div className="flex flex-col lg:flex-row items-center pb-6 gap-6 lg:gap-12 max-w-4xl">
-						<Image
-							src={HehMina}
-							alt="A smirking Mina."
-							className="h-32 lg:h-36 w-auto rounded-full border-neutral-50"
-						/>
-						<h1 className="text-neutral-50 font-display text-center md:text-left text-2xl md:text-3xl lg:text-5xl font-medium flex-grow">
+				<FadingImage
+					src={HehMina}
+					alt="A smirking Mina."
+					className="h-32 lg:h-44 w-auto rounded-full border-neutral-50"
+				/>
+				<div>
+					<Dialog.Title asChild>
+						<h2 className="lg:text-balance">
 							{t("Content.NSFW.Dialog.title")}
-						</h1>
-					</div>
-					<div className="text-center text-neutral-50 max-w-3xl">
-						<p>{t("Content.NSFW.Dialog.text1")}</p>
-						<p>{t("Content.NSFW.Dialog.text2")}</p>
-						<p className="font-bold text-red">{t("Content.NSFW.Dialog.text3")}</p>
-					</div>
-					<div className="flex flex-col md:flex-row items-center gap-3 pt-6 px-6">
-						<Dialog.Close asChild>
-							<Button
-								color="green"
-								onClick={() => {
-									params.set("nsfw", "show");
-									localStorage.setItem("confirmedNsfwDialog", "interestingly, yes");
-									localStorage.setItem("horny", "looks like it");
-								}}
-								disabled={timer < 10}
+							<span className="text-green">.</span>
+						</h2>
+					</Dialog.Title>
+					<Dialog.Description asChild>
+						<div>
+							<p>{t("Content.NSFW.Dialog.text1")}</p>
+							<p>{t("Content.NSFW.Dialog.text2")}</p>
+							<p className="font-bold text-red">{t("Content.NSFW.Dialog.text3")}</p>
+							<p
+								className={`text-neutral-700 lg:text-balance text-xs italic ${
+									timer < 10 ? "opacity-0 select-none" : "opacity-100"
+								} duration-250`}
 							>
-								<svg height={16} width={16} className="-rotate-90">
-									<a.circle
-										cx={8}
-										cy={8}
-										r={7}
-										strokeWidth={2}
-										className="stroke-neutral-950 fill-none"
-										strokeDasharray={44}
-										strokeDashoffset={progressCircle.val}
-										strokeLinecap="butt" // hehe butt
-									/>
-								</svg>
-								{t("Content.NSFW.Dialog.admitSins")}
-							</Button>
-						</Dialog.Close>
+								{t("Content.NSFW.Dialog.hint")}
+							</p>
+						</div>
+					</Dialog.Description>
+					<div className="flex flex-row flex-wrap justify-between sm:justify-normal gap-3 pt-3 pb-9 lg:pb-0">
+						<Button color="green" onClick={onAccept} disabled={timer < 10}>
+							<svg height={16} width={16} className="-rotate-90">
+								<a.circle
+									cx={8}
+									cy={8}
+									r={7}
+									strokeWidth={2}
+									className="stroke-neutral-950 fill-none"
+									strokeDasharray={44}
+									strokeDashoffset={progressCircle.val}
+									strokeLinecap="butt" // hehe butt
+								/>
+							</svg>
+							{t("Content.NSFW.Dialog.admitSins")}
+						</Button>
+
 						<Dialog.Close asChild>
 							<Button>{t("Content.NSFW.Dialog.nevermind")}</Button>
 						</Dialog.Close>
 					</div>
-					<p
-						className={`text-neutral text-xs italic max-w-xl text-center pt-6 ${
-							timer < 10 ? "opacity-0 select-none" : "opacity-100"
-						} duration-250`}
-					>
-						{t("Content.NSFW.Dialog.hint")}
-					</p>
-					<p className="lg:absolute pt-6 bottom-3 md:bottom-5 left-0 right-0 text-xs text-center">
-						{t.rich("Content.NSFW.Dialog.credit", {
-							Link: (chunks) => (
-								<Link href="https://twitter.com/wxsonz" target="_blank" className="text-link-external">
-									{chunks}
-								</Link>
-							),
-						})}
-					</p>
 				</div>
+				<Dialog.Close asChild>
+					<button
+						className="absolute inline-flex items-center justify-center top-3 md:top-5 right-3 md:right-5 size-10 rounded-full text-neutral-50 hover:bg-neutral-900 active:bg-neutral-800 duration-100"
+						aria-label="Close"
+					>
+						<X />
+					</button>
+				</Dialog.Close>
 			</Dialog.Content>
-		</>
+		</Dialog.Portal>
 	);
 }
