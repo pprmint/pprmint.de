252ab59a819689c96b6da3e3fb6db18e5cf9c841
pprmint <git@pprmint.art>
2024-03-13 22:57:41 +0100

    Extend props instead of reinventing the wheel.

--- src/components/ui/FadingImage.tsx
+++ src/components/ui/FadingImage.tsx
@@ -1,41 +1,20 @@
 "use client";
 import { MouseEventHandler, useState } from "react";
-import Image, { StaticImageData } from "next/image";
+import Image, { ImageProps, StaticImageData } from "next/image";
 import { a, useSpring } from "@react-spring/web";
 
-export default function FadingImage({
-	src,
-	alt,
-	width,
-	height,
-	className,
-	quality,
-	fill,
-	priority,
-}: {
-	src: string | StaticImageData;
-	alt: string;
-	width?: number;
-	height?: number;
+interface FadingImageProps extends ImageProps {
 	className?: string;
-	quality?: number;
-	fill?: boolean;
-	priority?: boolean;
-}) {
-	const [loaded, setLoaded] = useState(false);
+}
 
+export default function FadingImage({ className, ...rest }: FadingImageProps) {
+	const [loaded, setLoaded] = useState(false);
 	return (
 		<Image
-			src={src}
-			alt={alt}
-			fill={fill}
-			priority={priority}
-			width={width}
-			height={height}
-			quality={quality}
 			onLoad={() => setLoaded(true)}
 			className={`${loaded ? "opacity-100" : "opacity-0"} duration-500 ${className}`}
 			draggable={false}
+			{...rest}
 		/>
 	);
 }
