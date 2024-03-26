4831ad26a7916acfff723ef267d8e239326315c0
pprmint <git@pprmint.art>
2024-03-26 21:54:48 +0100

    Fix page jumping issue on closing, and stagger image fade in.

--- src/app/[locale]/gallery/gallery.tsx
+++ src/app/[locale]/gallery/gallery.tsx
@@ -1,27 +1,22 @@
 "use client";
 import * as Dialog from "@radix-ui/react-dialog";
-import { Fullscreen, Maximize, Minimize, X } from "lucide-react";
+import { Maximize, Minimize, X } from "lucide-react";
 import { useTranslations } from "next-intl";
 import { useState } from "react";
 import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import FadingImage from "src/components/ui/FadingImage";
-import Work, { Works } from "src/types/work";
+import { Works } from "src/types/work";
 
 export default function Gallery(works: { works: Works }) {
 	const t = useTranslations("GALLERY");
-	const [selected, setSelected] = useState(0);
-	const selectedWork = works.works.data[selected > works.works.data.length ? 0 : selected];
 	const [fullscreen, setFullscreen] = useState(false);
 	return (
-		<Dialog.Root>
-			<div className="mb-10 grid gap-3 grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 3xl:grid-cols-5">
-				{works.works.data.map((work: Work, index: number) => (
-					<Dialog.Trigger asChild key={work.id}>
-						<button
-							className="relative group overflow-hidden rounded-lg hover:contrast-75 active:contrast-100 active:opacity-75 duration-200 active:duration-75 cursor-pointer aspect-video focus-visible:animate-pulse"
-							onClick={() => setSelected(index)}
-						>
+		<div className="mb-10 grid gap-3 grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 3xl:grid-cols-5">
+			{works.works.data.map((work) => (
+				<Dialog.Root key={work.id}>
+					<Dialog.Trigger asChild>
+						<button className="relative group overflow-hidden rounded-lg hover:contrast-75 active:contrast-100 active:opacity-75 duration-200 active:duration-75 cursor-pointer aspect-video focus-visible:animate-pulse">
 							<FadingImage
 								src={`https://static.pprmint.art${work.attributes.cover.data.attributes.url}`}
 								width={work.attributes.cover.data.attributes.width}
@@ -31,91 +26,93 @@ export default function Gallery(works: { works: Works }) {
 							/>
 						</button>
 					</Dialog.Trigger>
-				))}
-			</div>
-			<Dialog.Portal>
-				<Dialog.Overlay className="bg-neutral-950/90 data-[state=open]:animate-fade-in data-[state=closed]:animate-fade-out fixed inset-0 z-50" />
-				<Dialog.Content
-					className={`fixed z-60 top-0 lg:top-1/2 left-0 lg:left-1/2 inset-0 lg:inset-auto ${
-						fullscreen
-							? "w-screen h-screen max-h-svh max-w-[100vw]"
-							: "max-h-svh lg:max-h-[90vh] lg:w-4/5-screen max-w-screen-3xl lg:border lg:ring-1 lg:rounded-xl"
-					} lg:-translate-x-1/2 lg:-translate-y-1/2 bg-neutral-950 border-neutral-900 ring-neutral-950 shadow-xl shadow-neutral-950/50 data-[state=open]:animate-scale-up data-[state=closed]:animate-scale-down focus-visible:outline-none origin-center lg:origin-top-left overflow-auto lg:duration-400 lg:ease-out-quint`}
-				>
-					<Dialog.Close asChild>
-						<button
-							className="absolute z-60 inline-flex items-center justify-center top-3 md:top-5 right-3 md:right-5 size-10 rounded-full text-neutral-50 hover:bg-neutral-900 active:bg-neutral-800 duration-100"
-							aria-label="Close"
+					<Dialog.Portal>
+						<Dialog.Overlay className="bg-neutral-950/90 data-[state=open]:animate-fade-in data-[state=closed]:animate-fade-out fixed inset-0 z-50" />
+						<Dialog.Content
+							className={`fixed z-60 top-0 lg:top-1/2 left-0 lg:left-1/2 inset-0 lg:inset-auto ${
+								fullscreen
+									? "w-screen h-screen max-h-svh max-w-[100vw]"
+									: "max-h-svh lg:max-h-[90vh] lg:w-4/5-screen max-w-screen-3xl lg:border lg:ring-1 lg:rounded-xl"
+							} lg:-translate-x-1/2 lg:-translate-y-1/2 bg-neutral-950 border-neutral-900 ring-neutral-950 shadow-xl shadow-neutral-950/50 data-[state=open]:animate-scale-up data-[state=closed]:animate-scale-down focus-visible:outline-none origin-center lg:origin-top-left overflow-auto lg:duration-400 lg:ease-out-quint`}
 						>
-							<X />
-						</button>
-					</Dialog.Close>
-					<button
-						className="absolute z-60 hidden lg:inline-flex items-center justify-center top-16 right-5 size-10 rounded-full text-neutral-50 hover:bg-neutral-900 active:bg-neutral-800 duration-100"
-						aria-label="Toggle fullscreen"
-						onClick={() => setFullscreen(!fullscreen)}
-					>
-						{fullscreen ? <Minimize /> : <Maximize />}
-					</button>
-					<div className="relative overflow-hidden h-80 md:h-1/3-screen">
-						<FadingImage
-							src={`https://static.pprmint.art${selectedWork.attributes.cover.data.attributes.formats.thumbnail.url}`}
-							alt={selectedWork.attributes.title}
-							fill
-							className={`object-cover ${selectedWork.attributes.coverFocus} h-full min-w-full contrast-[0.87] blur-lg`}
-						/>
-						<div
-							className="absolute inset-0"
-							style={{
-								backgroundImage: `url('data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" xml:space="preserve" width="24" height="24"><path d="M0 10V0h10a2 2 0 0 0 4 0h10v10a2 2 0 0 0 0 4v10H14a2 2 0 0 0-4 0H0V14a2 2 0 0 0 0-4Z" style="fill:%23111"/></svg>')`,
-								backgroundRepeat: "repeat",
-								backgroundPosition: "top",
-							}}
-						/>
-						<div className="absolute inset-0 flex items-end justify-start px-6 lg:px-9 pb-3 bg-gradient-to-t from-neutral-950 to-neutral-950/75">
-							<Dialog.Title asChild>
-								<h1>
-									{selectedWork.attributes.title}
-									<span className="text-green">.</span>
-								</h1>
-							</Dialog.Title>
-						</div>
-					</div>
-					<Dialog.Description asChild className="px-6 lg:px-9 prose-a:text-link-external">
-						{!!Object.hasOwn ? (
-							<Markdown remarkPlugins={[remarkGfm]}>{selectedWork.attributes.text}</Markdown>
-						) : (
-							<div>
-								<p>{selectedWork.attributes.text}</p>
-								<p className="text-xs bg-red-950 text-neutral-50 px-3 py-2 rounded-md w-fit">{t("Content.noHasOwn")}</p>
-							</div>
-						)}
-					</Dialog.Description>
-					<hr className="m-6 lg:m-9 border-neutral-900" />
-					{selectedWork.attributes.gallery.data.map((media) =>
-						media.attributes.mime.startsWith("image") ? (
-							<FadingImage
-								key={media.id.toString()}
-								src={`https://static.pprmint.art${media.attributes.url}`}
-								alt={media.attributes.alternativeText}
-								width={media.attributes.width}
-								height={media.attributes.height}
-								quality={100}
-								className="w-full h-auto animate-skeleton-pulse"
-							/>
-						) : (
-							media.attributes.mime.startsWith("video") && (
-								<video
-									controls
-									key={media.id.toString()}
-									className="w-full h-auto"
-									src={`https://static.pprmint.art${media.attributes.url}`}
+							<Dialog.Close asChild>
+								<button
+									className="absolute z-60 inline-flex items-center justify-center top-3 md:top-5 right-3 md:right-5 size-10 rounded-full text-neutral-50 hover:bg-neutral-900 active:bg-neutral-800 duration-100"
+									aria-label="Close"
+								>
+									<X />
+								</button>
+							</Dialog.Close>
+							<button
+								className="absolute z-60 hidden lg:inline-flex items-center justify-center top-16 right-5 size-10 rounded-full text-neutral-50 hover:bg-neutral-900 active:bg-neutral-800 duration-100"
+								aria-label="Toggle fullscreen"
+								onClick={() => setFullscreen(!fullscreen)}
+							>
+								{fullscreen ? <Minimize /> : <Maximize />}
+							</button>
+							<div className="relative overflow-hidden h-80 md:h-1/3-screen">
+								<FadingImage
+									src={`https://static.pprmint.art${work.attributes.cover.data.attributes.formats.thumbnail.url}`}
+									alt={work.attributes.title}
+									fill
+									className={`object-cover ${work.attributes.coverFocus} h-full min-w-full contrast-[0.87] blur-lg`}
 								/>
-							)
-						)
-					)}
-				</Dialog.Content>
-			</Dialog.Portal>
-		</Dialog.Root>
+								<div
+									className="absolute inset-0"
+									style={{
+										backgroundImage: `url('data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" xml:space="preserve" width="24" height="24"><path d="M0 10V0h10a2 2 0 0 0 4 0h10v10a2 2 0 0 0 0 4v10H14a2 2 0 0 0-4 0H0V14a2 2 0 0 0 0-4Z" style="fill:%23111"/></svg>')`,
+										backgroundRepeat: "repeat",
+										backgroundPosition: "top",
+									}}
+								/>
+								<div className="absolute inset-0 flex items-end justify-start px-6 lg:px-9 pb-3 bg-gradient-to-t from-neutral-950 to-neutral-950/75">
+									<Dialog.Title asChild>
+										<h1>
+											{work.attributes.title}
+											<span className="text-green">.</span>
+										</h1>
+									</Dialog.Title>
+								</div>
+							</div>
+							<Dialog.Description asChild className="px-6 lg:px-9 prose-a:text-link-external">
+								{!!Object.hasOwn ? (
+									<Markdown remarkPlugins={[remarkGfm]}>{work.attributes.text}</Markdown>
+								) : (
+									<div>
+										<p>{work.attributes.text}</p>
+										<p className="text-xs bg-red-950 text-neutral-50 px-3 py-2 rounded-md w-fit">
+											{t("Content.noHasOwn")}
+										</p>
+									</div>
+								)}
+							</Dialog.Description>
+							<hr className="m-6 lg:m-9 border-neutral-900" />
+							{work.attributes.gallery.data.map((media) =>
+								media.attributes.mime.startsWith("image") ? (
+									<FadingImage
+										key={media.id.toString()}
+										src={`https://static.pprmint.art${media.attributes.url}`}
+										alt={media.attributes.alternativeText}
+										width={media.attributes.width}
+										height={media.attributes.height}
+										quality={100}
+										className="w-full h-auto animate-skeleton-pulse"
+									/>
+								) : (
+									media.attributes.mime.startsWith("video") && (
+										<video
+											controls
+											key={media.id.toString()}
+											className="w-full h-auto"
+											src={`https://static.pprmint.art${media.attributes.url}`}
+										/>
+									)
+								)
+							)}
+						</Dialog.Content>
+					</Dialog.Portal>
+				</Dialog.Root>
+			))}
+		</div>
 	);
 }
--- src/app/[locale]/mina/gallery.tsx
+++ src/app/[locale]/mina/gallery.tsx
@@ -1,39 +1,30 @@
 "use client";
 import { SiInstagram, SiTwitter, SiYoutube } from "@icons-pack/react-simple-icons";
 import * as Dialog from "@radix-ui/react-dialog";
-import { useTransition, a, config, easings } from "@react-spring/web";
+import { useTransition } from "@react-spring/web";
 import { EyeOff, Globe, X } from "lucide-react";
 import { useTranslations } from "next-intl";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import FadingImage from "src/components/ui/FadingImage";
 import { Link } from "src/navigation";
-import MinaArtwork, { MinaArtworks } from "src/types/mina-artwork";
+import { MinaArtworks } from "src/types/mina-artwork";
 
 export default function Gallery(artworks: { artworks: MinaArtworks }) {
 	const t = useTranslations("MINA");
-	const [open, setOpen] = useState(false);
-	const [selected, setSelected] = useState(0);
-	const selectedArtwork = artworks.artworks.data[selected > artworks.artworks.data.length ? 0 : selected];
 	const [selectedVariant, setSelectedVariant] = useState(0);
 
 	// Reset to 0 after the lightbox is closed.
-	// Avoids errors when navigating between pages if a variant was selected.
-	useEffect(() => {
-		!open &&
-			setTimeout(() => {
-				setSelected(0);
-				setSelectedVariant(0);
-			}, 200);
-	}, [open]);
+	function handleClose() {
+		setTimeout(() => {
+			setSelectedVariant(0);
+		}, 200);
+	}
 	return (
-		<Dialog.Root open={open} onOpenChange={setOpen}>
-			<div className="mb-10 grid gap-3 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
-				{artworks.artworks.data.map((art: MinaArtwork, index) => (
-					<Dialog.Trigger asChild key={art.id}>
-						<button
-							className="relative group overflow-hidden rounded-lg hover:contrast-75 active:contrast-100 active:opacity-75 duration-200 active:duration-75 cursor-pointer aspect-square focus-visible:animate-pulse"
-							onClick={() => setSelected(index)}
-						>
+		<div className="mb-10 grid gap-3 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+			{artworks.artworks.data.map((art, index) => (
+				<Dialog.Root key={art.id} onOpenChange={handleClose}>
+					<Dialog.Trigger asChild>
+						<button className="relative group overflow-hidden rounded-lg hover:contrast-75 active:contrast-100 active:opacity-75 duration-200 active:duration-75 cursor-pointer aspect-square focus-visible:animate-pulse">
 							<FadingImage
 								src={`https://static.pprmint.art${art.attributes.artwork.data[0].attributes.url}`}
 								width={art.attributes.artwork.data[0].attributes.width}
@@ -43,6 +34,7 @@ export default function Gallery(artworks: { artworks: MinaArtworks }) {
 									art.attributes.nsfw && "blur-lg group-hover:blur-none opacity-50 group-hover:opacity-100 duration-200"
 								}`}
 								unoptimized={art.attributes.artwork.data[0].attributes.url.includes(".gif")}
+                                style={{transitionDelay: `${index / 100}s`}}
 							/>
 							{art.attributes.nsfw && (
 								<EyeOff
@@ -52,87 +44,85 @@ export default function Gallery(artworks: { artworks: MinaArtworks }) {
 							)}
 						</button>
 					</Dialog.Trigger>
-				))}
-			</div>
-			<Dialog.Portal>
-				<Dialog.Overlay className="bg-neutral-950 data-[state=open]:animate-fade-in data-[state=closed]:animate-fade-out fixed inset-0 z-50" />
-				<Dialog.Content
-					className={`fixed z-60 flex flex-col gap-12 items-center top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 max-h-svh w-screen max-w-max data-[state=open]:animate-scale-up data-[state=closed]:animate-scale-down origin-top-left overflow-clip duration-200 focus-visible:outline-none`}
-				>
-					<FadingImage
-						src={`https://static.pprmint.art${selectedArtwork.attributes.artwork.data[selectedVariant].attributes.url}`}
-						width={selectedArtwork.attributes.artwork.data[selectedVariant].attributes.width}
-						height={selectedArtwork.attributes.artwork.data[selectedVariant].attributes.height}
-						alt=""
-						className="max-h-svh max-w-[90vw] w-auto py-16"
-						unoptimized
-					/>
-					<div className="absolute flex items-center top-0 inset-x-0 h-16 text-xl font-display">
-						<div className="flex items-center flex-grow gap-3">
-							<Dialog.Title asChild>
-								<p>
-									{t("Content.Artworks.drawnBy")}
-									<span className="font-semibold text-neutral-50">
-										{selectedArtwork.attributes.artist.data.attributes.name}
-									</span>
-									{selectedArtwork.attributes.heart && <span className="text-red"> ♥</span>}
-								</p>
-							</Dialog.Title>
-							{selectedArtwork.attributes.artist.data.attributes.creditUrl && (
-								<Link
-									href={selectedArtwork.attributes.artist.data.attributes.creditUrl!}
-									target="_blank"
-									rel="noopener noreferrer"
-									className="rounded-full"
-								>
-									<button
-										tabIndex={-1}
-										className="text-neutral-50 inline-flex items-center justify-center size-9 rounded-full bg-neutral-50/10 hover:bg-neutral-50/20 duration-100 text-xl"
-									>
-										{selectedArtwork.attributes.artist.data.attributes.creditUrl!.startsWith("https://twitter.com/") ? (
-											<SiTwitter size={16} />
-										) : selectedArtwork.attributes.artist.data.attributes.creditUrl!.startsWith(
-												"https://www.instagram.com/"
-										  ) ? (
-											<SiInstagram size={16} />
-										) : selectedArtwork.attributes.artist.data.attributes.creditUrl!.startsWith(
-												"https://www.youtube.com/"
-										  ) ? (
-											<SiYoutube size={16} />
-										) : (
-											<Globe size={16} />
-										)}
+					<Dialog.Portal>
+						<Dialog.Overlay className="bg-neutral-950 data-[state=open]:animate-fade-in data-[state=closed]:animate-fade-out fixed inset-0 z-50" />
+						<Dialog.Content
+							className={`fixed z-60 flex flex-col gap-12 items-center top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 max-h-svh w-screen max-w-max data-[state=open]:animate-scale-up data-[state=closed]:animate-scale-down origin-top-left overflow-clip duration-200 focus-visible:outline-none`}
+						>
+							<FadingImage
+								src={`https://static.pprmint.art${art.attributes.artwork.data[selectedVariant]?.attributes.url}`}
+								width={art.attributes.artwork.data[selectedVariant]?.attributes.width}
+								height={art.attributes.artwork.data[selectedVariant]?.attributes.height}
+								alt=""
+								className="max-h-svh max-w-[90vw] w-auto py-16"
+								unoptimized
+							/>
+							<div className="absolute flex items-center top-0 inset-x-0 h-16 text-xl font-display">
+								<div className="flex items-center flex-grow gap-3">
+									<Dialog.Title asChild>
+										<p>
+											{t("Content.Artworks.drawnBy")}
+											<span className="font-semibold text-neutral-50">
+												{art.attributes.artist.data.attributes.name}
+											</span>
+											{art.attributes.heart && <span className="text-red"> ♥</span>}
+										</p>
+									</Dialog.Title>
+									{art.attributes.artist.data.attributes.creditUrl && (
+										<Link
+											href={art.attributes.artist.data.attributes.creditUrl!}
+											target="_blank"
+											rel="noopener noreferrer"
+											className="rounded-full"
+										>
+											<button
+												tabIndex={-1}
+												className="text-neutral-50 inline-flex items-center justify-center size-9 rounded-full bg-neutral-50/10 hover:bg-neutral-50/20 duration-100 text-xl"
+											>
+												{art.attributes.artist.data.attributes.creditUrl!.startsWith("https://twitter.com/") ? (
+													<SiTwitter size={16} />
+												) : art.attributes.artist.data.attributes.creditUrl!.startsWith(
+														"https://www.instagram.com/"
+												  ) ? (
+													<SiInstagram size={16} />
+												) : art.attributes.artist.data.attributes.creditUrl!.startsWith("https://www.youtube.com/") ? (
+													<SiYoutube size={16} />
+												) : (
+													<Globe size={16} />
+												)}
+											</button>
+										</Link>
+									)}
+								</div>
+								<Dialog.Close asChild>
+									<button className="text-neutral-50 inline-flex items-center justify-center size-9 rounded-full bg-neutral-50/10 hover:bg-neutral-50/20 duration-100 text-xl">
+										<X />
 									</button>
-								</Link>
+								</Dialog.Close>
+							</div>
+							{art.attributes.artwork.data.length >= 2 && (
+								<div className="absolute flex items-center justify-center bottom-0 inset-x-0 h-16">
+									{art.attributes.artwork.data.map((variant, index) => (
+										<button
+											key={index}
+											className={`group h-full ${
+												index === selectedVariant ? "w-16" : "w-9"
+											} px-2 duration-200 ease-out-quint`}
+											onClick={() => setSelectedVariant(index)}
+										>
+											<div
+												className={`h-2 ${
+													index === selectedVariant ? "bg-neutral-50" : "bg-neutral-800 group-hover:bg-neutral-700"
+												} rounded-full duration-200 ease-out-quint`}
+											/>
+										</button>
+									))}
+								</div>
 							)}
-						</div>
-						<Dialog.Close asChild>
-							<button className="text-neutral-50 inline-flex items-center justify-center size-9 rounded-full bg-neutral-50/10 hover:bg-neutral-50/20 duration-100 text-xl">
-								<X />
-							</button>
-						</Dialog.Close>
-					</div>
-					{selectedArtwork.attributes.artwork.data.length >= 2 && (
-						<div className="absolute flex items-center justify-center bottom-0 inset-x-0 h-16">
-							{selectedArtwork.attributes.artwork.data.map((variant, index) => (
-								<button
-									key={index}
-									className={`group h-full ${
-										index == selectedVariant ? "w-16" : "w-9"
-									} px-2 duration-200 ease-out-quint`}
-									onClick={() => setSelectedVariant(index)}
-								>
-									<div
-										className={`h-2 ${
-											index == selectedVariant ? "bg-neutral-50" : "bg-neutral-800 group-hover:bg-neutral-700"
-										} rounded-full duration-200 ease-out-quint`}
-									/>
-								</button>
-							))}
-						</div>
-					)}
-				</Dialog.Content>
-			</Dialog.Portal>
-		</Dialog.Root>
+						</Dialog.Content>
+					</Dialog.Portal>
+				</Dialog.Root>
+			))}
+		</div>
 	);
 }
