cd09d2cc1a48a0c8d775d114a83094a8c67a6ec8
pprmint <git@pprmint.art>
2024-04-12 23:45:13 +0200

    Further tweak design of these cards.

--- src/app/[locale]/commissions/overview.tsx
+++ src/app/[locale]/commissions/overview.tsx
@@ -62,14 +62,18 @@ export default function Overview(data: { data: Commission }) {
 				].map((service) => (
 					<Collapsible.Root
 						key={service.id}
-						className="group/card relative w-full h-fit data-[state='closed']:active:scale-[0.98] data-[state='closed']:ring-1 ring-inset ring-neutral-900 hover:bg-neutral-900 data-[state='open']:bg-neutral-900 duration-200 data-[state='closed']:active:duration-100 rounded-lg overflow-clip"
+						className="group/card relative w-full h-fit data-[state=closed]:hover:bg-neutral-900 data-[state=closed]:active:scale-[0.98] data-[state=closed]:active:opacity-75 ring-1 ring-inset ring-neutral-900 duration-200 data-[state=closed]:active:duration-100 rounded-xl overflow-clip"
 					>
-						<Collapsible.Trigger className="w-full selection:text-neutral-950">
-							<div className={`w-full right-0 h-2 ${service.color}`} />
-							<div className="flex flex-col-reverse lg:flex-row px-6 group-data-[state='closed']/card:pb-12 group-data-[state='open']/card:pb-6 items-center text-center lg:text-left duration-300 ease-out-cubic">
+						<Collapsible.Trigger className="w-full selection:text-neutral-950 focus-visible:bg-neutral-900 data-[state=open]:hover:bg-neutral-900 data-[state=open]:active:opacity-75 duration-200">
+							<div
+								className={`w-full right-0 group-data-[state=closed]/card:h-0 group-data-[state=open]/card:h-1.5 duration-500 ease-out-quint ${service.color}`}
+							/>
+							<div className="flex flex-col-reverse lg:flex-row px-6 group-data-[state=closed]/card:pb-14 group-data-[state=open]/card:pb-6 items-center text-center lg:text-left duration-500 ease-out-quint">
 								<div className="flex-grow pt-6">
 									<h3>{t(`Content.Offers.${service.id}.heading`)}</h3>
-									<p className="text-md">{t(`Content.Offers.${service.id}.description`)}</p>
+									<p className="text-md">
+										{t(`Content.Offers.${service.id}.description`)}
+									</p>
 									<p className="font-display-mono font-semibold text-neutral-50 text-2xl lg:text-4xl tracking-tighter">
 										{service.price}
 									</p>
@@ -78,43 +82,53 @@ export default function Overview(data: { data: Commission }) {
 							</div>
 						</Collapsible.Trigger>
 						<Collapsible.Content className="data-[state=open]:animate-collapsible-vertical-open data-[state=closed]:animate-collapsible-vertical-close">
-							<div className="border-t border-neutral-800">
-								<h4 className="px-6 py-3">{t("Content.Offers.need")}</h4>
-								<ul>
-									{service.need.map((need) => (
-										<li key={need} className="flex gap-3 items-center odd:bg-neutral-800 px-6 py-2">
-											<Plus size={16} />
-											{t(`Content.Offers.${service.id}.Need.${need}`)}
-										</li>
-									))}
-								</ul>
-								<h4 className="border-t border-neutral-800 px-6 py-3">{t("Content.Offers.get")}</h4>
-								<ul className="pb-12">
-									{service.get.map((get) => (
-										<li key={get} className="flex gap-3 items-center odd:bg-neutral-800 px-6 py-2">
-											<Check size={16} />
-											{t(`Content.Offers.${service.id}.Get.${get}`)}
-										</li>
-									))}
-								</ul>
-							</div>
+							<hr className="border-neutral-900" />
+							<h4 className="px-6 py-3">{t("Content.Offers.need")}</h4>
+							<ul>
+								{service.need.map((need) => (
+									<li
+										key={need}
+										className="flex gap-3 items-center odd:bg-neutral-900 px-6 py-1.5"
+									>
+										<Plus size={16} />
+										{t(`Content.Offers.${service.id}.Need.${need}`)}
+									</li>
+								))}
+							</ul>
+							<hr className="border-neutral-900" />
+							<h4 className="px-6 py-3">{t("Content.Offers.get")}</h4>
+							<ul>
+								{service.get.map((get) => (
+									<li
+										key={get}
+										className="flex gap-3 items-center odd:bg-neutral-900 px-6 py-1.5"
+									>
+										<Check size={16} />
+										{t(`Content.Offers.${service.id}.Get.${get}`)}
+									</li>
+								))}
+							</ul>
+							<hr className="border-neutral-900 pb-14" />
 						</Collapsible.Content>
 						<Tooltip.Root delayDuration={300}>
 							<Collapsible.Trigger
-								className="group/arrow absolute bottom-0 w-full h-12 flex items-center justify-between bg-gradient-to-t from-neutral-900"
+								className="group/arrow absolute bottom-0 w-full h-14 flex items-center justify-between data-[state=open]:hover:bg-neutral-900 data-[state=open]:active:opacity-75 duration-200"
 								aria-hidden
+								tabIndex={-1}
 							>
 								<ChevronDown
 									size={16}
 									strokeWidth={3}
-									className="ml-6 group-data-[state='open']/card:rotate-180 group-data-[state='closed']/card:group-hover/card:translate-y-1 group-data-[state='open']/card:group-hover/arrow:-translate-y-1 duration-200 ease-out-cubic"
+									className="ml-6 group-data-[state=open]/card:rotate-180 group-data-[state=closed]/card:group-hover/card:translate-y-1 group-data-[state=open]/card:group-hover/arrow:-translate-y-1 duration-250 ease-out-cubic"
 								/>
 								<Tooltip.Trigger asChild>
 									<div className="flex gap-2 px-6 py-4">
 										{[...Array(service.totalSlots)].map((_, index) => (
 											<div
 												className={`size-3 border-2 ${
-													index >= service.filledSlots ? "bg-neutral-50" : "border-neutral"
+													index >= service.filledSlots
+														? "bg-neutral-50"
+														: "border-neutral"
 												} rounded-full`}
 												key={index}
 											/>
@@ -124,11 +138,13 @@ export default function Overview(data: { data: Commission }) {
 								<Tooltip.Content
 									className="data-[state=delayed-open]:animate-tooltip-enter-left data-[state=instant-open]:animate-tooltip-enter-left
                                         data-[state=closed]:animate-tooltip-exit-left
-                                        select-none rounded-full border border-neutral-700 bg-neutral-800 px-3 py-2 text-sm leading-none shadow-lg will-change-[transform,opacity]"
+                                        select-none rounded-full border border-neutral-700 bg-neutral-800 px-3 py-2 text-sm leading-none shadow-xl shadow-neutral-950/50"
 									side="left"
 									sideOffset={-5}
 								>
-									{t("Content.Offers.slotsAvailable", { count: service.totalSlots - service.filledSlots })}
+									{t("Content.Offers.slotsAvailable", {
+										count: service.totalSlots - service.filledSlots,
+									})}
 									<Tooltip.Arrow className="fill-neutral-700" />
 								</Tooltip.Content>
 							</Collapsible.Trigger>
