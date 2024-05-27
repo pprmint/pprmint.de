e0a00ee602a23a0dd0fba7f7271645afb1cd1e75
pprmint <git@pprmint.art>
2024-05-27 22:58:41 +0200

    yea

--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -8,15 +8,17 @@ import Wordmark from "public/assets/wordmark.svg";
 
 export default function NavBar() {
 	return (
-		<div
-			className={`z-90 fixed w-full h-16 flex items-start duration-300`}
-		>
-            <div
-				style={{ maskImage: "linear-gradient(rgba(0,0,0,1) 0%, rgba(0,0,0,.4) 50%, rgba(0,0,0,0) 100%)", maskRepeat: "space", backgroundRepeat: "repeat" }}
+		<div className={`z-90 fixed w-full h-16 flex items-start duration-300`}>
+			<div
+				style={{
+					maskImage: "linear-gradient(rgba(0,0,0,1) 0%, rgba(0,0,0,.4) 50%, rgba(0,0,0,0) 100%)",
+					maskRepeat: "space",
+					backgroundRepeat: "repeat",
+				}}
 				className="fixed z-70 inset-x-0 top-0 h-32 bg-neutral-950/75 backdrop-blur-md pointer-events-none"
 			/>
-			<Link href="/" className="z-80 pl-3 md:pl-6 my-auto mr-auto drop-shadow-[0px_2px_12px_#1118]">
-				<Image src={Wordmark} alt="pprmint. logo" className="h-9 w-[184px] mt-1" />
+			<Link href="/" className="z-80 pl-6 md:pl-9 my-auto mr-auto drop-shadow-[0px_2px_12px_#1118]">
+				<Image src={Wordmark} alt="pprmint. logo" className="h-auto w-[173px] mt-1" />
 			</Link>
 			<div className="hidden z-80 md:flex">
 				<DesktopNavigation />
