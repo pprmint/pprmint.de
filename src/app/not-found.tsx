e7c3e4e885105fa641195354e947c0489ce435be
pprmint <git@pprmint.de>
2024-08-24 12:25:33 +0200

    Replace background font on 404 page.

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -33,7 +33,11 @@ export default function NotFound() {
 	const fadeIn = useSpring({
 		from: { opacity: 0 },
 		to: { opacity: 0.75 },
+		config: {
+			duration: 300,
+		},
 	});
+	const [countFinished, setCountFinished] = useState(false);
 	const countUp = useSpring({
 		from: { val: 100 },
 		to: { val: 404 },
@@ -41,6 +45,7 @@ export default function NotFound() {
 			tension: 150,
 			friction: 40,
 		},
+		onRest: () => setCountFinished(true),
 	});
 	const ripple = useSpring({
 		from: { opacity: 0.75, scale: 0 },
@@ -63,29 +68,31 @@ export default function NotFound() {
 
 	const cycleTransition = useTransition(!showGerman, {
 		from: { opacity: 0, y: 20 },
-		enter: { opacity: 1, y: 0 },
-		leave: { opacity: 0, y: -20, config: config.stiff },
+		enter: { opacity: 1, y: 0, config: { duration: 500, easing: easings.easeOutBack } },
+		leave: { opacity: 0, y: -40, config: { duration: 400, easing: easings.easeInBack } },
 		exitBeforeEnter: true,
 	});
 
 	return (
 		<html lang="en">
-			<body className="bg-neutral-950 selection:bg-orange selection:text-neutral-950 text-neutral focus-visible:outline-none focus-visible:ring-2 overflow-x-hidden">
-				<Link href="/" className="fixed top-0 left-0 z-50 pl-3 md:pl-6 h-16">
-					<Image src={Wordmark} alt="pprmint. logo" className="h-9 w-[184px] mt-4" />
+			<body className="bg-neutral-950 selection:bg-green selection:text-neutral-950 text-neutral focus-visible:outline-none focus-visible:ring-2 overflow-x-hidden">
+				<Link href="/" className="fixed top-0 left-0 z-50 pl-3 md:pl-6 h-16 flex items-center">
+					<Image src={Wordmark} alt="pprmint. logo" className="h-auto w-[150px] mt-1" />
 				</Link>
 				<main className="relative w-screen h-screen overflow-clip">
-					<div className="absolute inset-0">
+					<a.div className="absolute inset-0" style={{ ...fadeIn }}>
 						<a.div
-							className="absolute top-1/2 -translate-y-1/2 text-orange-800 font-display-mono font-light blur-sm lg:blur-lg w-full text-center -skew-y-6"
-							style={{ fontSize: "50vw", ...fadeIn }}
+							className={`absolute top-1/2 -translate-y-1/2 ${
+								countFinished ? "text-neutral-800" : "text-green-800"
+							} font-digits blur-sm lg:blur-lg w-full text-center -skew-y-6 duration-500`}
+							style={{ fontSize: "50vw" }}
 						>
 							{countUp.val.to((val) => Math.floor(val))}
 						</a.div>
 						<div className="absolute flex items-center justify-center top-0 left-0 w-screen h-screen">
 							<a.div
 								style={{ ...ripple }}
-								className="bg-orange-800 aspect-square h-screen md:h-auto md:w-screen rounded-full"
+								className="bg-green-800 aspect-square h-screen md:h-auto md:w-screen rounded-full"
 							/>
 						</div>
 						<div
@@ -95,7 +102,7 @@ export default function NotFound() {
 								backgroundRepeat: "repeat",
 							}}
 						/>
-					</div>
+					</a.div>
 					<a.div
 						className="z-10 flex flex-col md:flex-row-reverse justify-center items-center gap-3 md:gap-6 w-full max-w-7xl h-full mx-auto px-6 md:px-9"
 						style={{ ...inFromBottom }}
@@ -116,22 +123,30 @@ export default function NotFound() {
 										<div className="pb-6">
 											<h1>
 												She looked everywhere
-												<span className="text-orange">.</span>
+												<span className="text-green">.</span>
 											</h1>
 											<p className="md:text-lg">
-												There's no page here. Maybe it was moved or it never existed. That's how it is sometimes.
+												There's no page here. Maybe it was moved or it never existed. That's how
+												it is sometimes.
 											</p>
 										</div>
 										<Link href="/" className="w-fit">
-											<Button color="orange">
+											<Button color="green">
 												Go to home page
 												<Home />
 											</Button>
 										</Link>
 									</a.div>
-									<a.p style={styles} className="md:absolute pt-6 bottom-3 md:bottom-5 left-0 right-0 text-xs text-center">
+									<a.p
+										style={styles}
+										className="md:absolute pt-6 bottom-3 md:bottom-5 left-0 right-0 text-xs text-center"
+									>
 										Detective Mina was drawn by{" "}
-										<Link href="https://twitter.com/DIVAOFDESPAlR" target="_blank" className="text-link-external">
+										<Link
+											href="https://twitter.com/DIVAOFDESPAlR"
+											target="_blank"
+											className="text-link-external"
+										>
 											Layer (@DIVAOFDESPAlR)
 										</Link>
 										.
@@ -145,23 +160,30 @@ export default function NotFound() {
 									>
 										<div className="pb-6">
 											<h1>
-												Sie hat überall gesucht<span className="text-orange">.</span>
+												Sie hat überall gesucht<span className="text-green">.</span>
 											</h1>
 											<p className="md:text-lg">
-												Hier gibt es keine Seite. Vielleicht wurde sie verschoben, oder es gab sie nie. So ist das
-												manchmal.
+												Hier gibt es keine Seite. Vielleicht wurde sie verschoben, oder es gab
+												sie nie. So ist das manchmal.
 											</p>
 										</div>
 										<Link href="/" className="w-fit">
-											<Button color="orange">
+											<Button color="green">
 												Zur Startseite
 												<Home />
 											</Button>
 										</Link>
 									</a.div>
-									<a.p style={styles} className="md:absolute pt-6 bottom-3 md:bottom-5 left-0 right-0 text-xs text-center">
+									<a.p
+										style={styles}
+										className="md:absolute pt-6 bottom-3 md:bottom-5 left-0 right-0 text-xs text-center"
+									>
 										Detektiv Mina wurde von{" "}
-										<Link href="https://twitter.com/DIVAOFDESPAlR" target="_blank" className="text-link-external">
+										<Link
+											href="https://twitter.com/DIVAOFDESPAlR"
+											target="_blank"
+											className="text-link-external"
+										>
 											Layer (@DIVAOFDESPAlR)
 										</Link>{" "}
 										gezeichnet.
