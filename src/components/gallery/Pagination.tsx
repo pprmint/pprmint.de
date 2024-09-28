6a042285aab849bbf03e88456f28c54a1c2e0611
pprmint <git@pprmint.de>
2024-09-28 15:54:55 +0200

    Improve handling of several pages + scaling on mobile.

--- src/components/gallery/Pagination.tsx
+++ src/components/gallery/Pagination.tsx
@@ -1,21 +1,21 @@
 "use client";
+import dynamic from "next/dynamic";
 import { useSearchParams } from "next/navigation";
+import { useMediaQuery } from "react-responsive";
 import ChevronLeft from "src/icons/ChevronLeft";
 import ChevronRight from "src/icons/ChevronRight";
 import { usePathname, useRouter } from "src/navigation";
 
-export default function Pagination({
-	page,
-	pageCount,
-	onPageChange,
-}: {
-	page: number;
-	pageCount: number;
-	onPageChange?: () => void;
-}) {
+function Pagination({ page, pageCount, onPageChange }: { page: number; pageCount: number; onPageChange?: () => void }) {
 	const searchParams = useSearchParams();
 	const pathname = usePathname();
 	const { replace } = useRouter();
+
+	const buttonRange = useMediaQuery({ minWidth: 768 }) ? 6 : 4;
+	const buttonOffset = useMediaQuery({ minWidth: 768 }) ? 3 : 2;
+	const buttonStart = Math.max(1, Math.min(pageCount - buttonRange, page - (buttonRange - buttonOffset)));
+	const buttonEnd = Math.min(pageCount - 1, buttonStart + buttonRange - 1);
+
 	function handlePagination(page: string) {
 		const params = new URLSearchParams(searchParams);
 		if (page) {
@@ -28,29 +28,52 @@ export default function Pagination({
 	}
 
 	return (
-		<div className="flex gap-2 justify-center w-full">
+		<div className="flex gap-1 justify-center items-center w-full">
 			<button
-				className={`group inline-flex items-center justify-center size-10 rounded-full text-lg text-neutral-50 hover:bg-neutral-900 active:bg-neutral-800 disabled:text-neutral-800 disabled:bg-transparent duration-100`}
+				className={`group inline-flex items-center justify-center size-8 sm:size-10 sm:text-lg rounded-full text-neutral-50 hover:bg-neutral-900 active:bg-neutral-800 disabled:text-neutral-800 disabled:bg-transparent duration-100`}
 				disabled={page == 1}
 				onClick={() => handlePagination(String(page - 1))}
 			>
 				<ChevronLeft className={`${page != 1 && "group-active:-translate-x-0.5"} duration-50`} />
 			</button>
-			{[...Array(pageCount)].map((_, index) => (
-				<button
-					key={index}
-					className={`size-10 rounded-full text-lg ${
-						index + 1 == page
-							? "text-neutral-950 bg-neutral-50 pointer-events-none"
-							: "text-neutral-50 hover:bg-neutral-900 active:bg-neutral-800"
-					} duration-100`}
-					onClick={() => handlePagination(String(index + 1))}
-				>
-					{index + 1}
-				</button>
-			))}
 			<button
-				className={`group inline-flex items-center justify-center size-10 rounded-full text-lg text-neutral-50 hover:bg-neutral-900 active:bg-neutral-800 disabled:text-neutral-800 disabled:bg-transparent duration-100`}
+				className={`size-8 sm:size-10 sm:text-lg rounded-full ${
+					1 == page
+						? "text-neutral-950 bg-neutral-50 pointer-events-none font-bold"
+						: "text-neutral-50 hover:bg-neutral-900 active:bg-neutral-800"
+				}`}
+				onClick={() => handlePagination(String(1))}
+			>
+				1
+			</button>
+			{[...Array(pageCount)].map((_, index) => {
+				if (index < buttonStart || index >= buttonEnd) return null;
+				return (
+					<button
+						key={index}
+						className={`size-8 sm:size-10 sm:text-lg rounded-full ${
+							index + 1 == page
+								? "text-neutral-950 bg-neutral-50 pointer-events-none font-bold"
+								: "text-neutral-50 hover:bg-neutral-900 active:bg-neutral-800"
+						}`}
+						onClick={() => handlePagination(String(index + 1))}
+					>
+						{index + 1}
+					</button>
+				);
+			})}
+			<button
+				className={`size-8 sm:size-10 sm:text-lg rounded-full ${
+					pageCount == page
+						? "text-neutral-950 bg-neutral-50 pointer-events-none font-bold"
+						: "text-neutral-50 hover:bg-neutral-900 active:bg-neutral-800"
+				}`}
+				onClick={() => handlePagination(String(pageCount))}
+			>
+				{pageCount}
+			</button>
+			<button
+				className={`group inline-flex items-center justify-center size-8 sm:size-10 sm:text-lg rounded-full text-neutral-50 hover:bg-neutral-900 active:bg-neutral-800 disabled:text-neutral-800 disabled:bg-transparent duration-100`}
 				disabled={page == pageCount}
 				onClick={() => handlePagination(String(page + 1))}
 			>
@@ -59,3 +82,7 @@ export default function Pagination({
 		</div>
 	);
 }
+
+export default dynamic(() => Promise.resolve(Pagination), {
+	ssr: false,
+});
