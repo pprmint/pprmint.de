7874d0314367ed3c94421c11951185bf13e5c8fa
Nicklas <git@pprmint.de>
2024-11-16 13:47:14 +0100

    Make English the default.

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -1,4 +1,4 @@
 export type Locale = (typeof locales)[number];
 
 export const locales = ['en', 'de'] as const;
-export const defaultLocale: Locale = 'de';
+export const defaultLocale: Locale = 'en';
