8a4d9a18a211711d334a825c429bcc07f4a8985d
pprmint <git@pprmint.de>
2024-11-17 04:08:18 +0100

    Trade offer page.

---
+++ src/app/tradeoffer/widget.tsx
@@ -0,0 +1,76 @@
+"use client";
+import { useState } from "react";
+import { useTransition, a } from "@react-spring/web";
+import Button from "src/components/ui/Button";
+import Kofi from "src/icons/Kofi";
+import { useTranslations } from "next-intl";
+import Link from "next/link";
+import ExternalLink from "src/icons/ExternalLink";
+
+export default function KofiWidget() {
+	const t = useTranslations("");
+	const [visible, setVisible] = useState(false);
+	const transition = useTransition(visible, {
+		from: {
+			opacity: 0,
+		},
+		enter: {
+			opacity: 1,
+			scale: 1,
+		},
+		leave: {
+			opacity: 0,
+			scale: 0.9,
+		},
+		exitBeforeEnter: true,
+	});
+	return (
+		<div className="w-full max-w-[400px]">
+			<div className="rounded-xl shadow-lg mx-auto w-full max-w-[400px] h-[640px] overflow-clip bg-elevate">
+				{transition((style, item) =>
+					item ? (
+						<a.iframe
+							style={style}
+							// @ts-expect-error
+							id="kofiframe"
+							src="https://ko-fi.com/pprmint/?hidefeed=true&widget=true&embed=true"
+							height="640"
+							title="pprmint"
+							className="w-full"
+						/>
+					) : (
+						// @ts-expect-error
+						<a.div
+							style={style}
+							className="flex flex-col items-center justify-center size-full p-6 text-center"
+						>
+							<Kofi className="size-[60px] text-neutral-50" />
+							<p>{t("KOFI.dataInfo")}</p>
+							<Link
+								className="text-sm mt-3 mb-6 text-link-external inline-flex gap-1 items-center"
+								href="https://more.ko-fi.com/privacy"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								{t("PRIVACY.Content.privacyPolicyOf", { provider: "Ko-fi" })}
+								<ExternalLink />
+							</Link>
+							<Button outlined onClick={() => setVisible(true)}>
+								{t("KOFI.showWidget")}
+							</Button>
+						</a.div>
+					)
+				)}
+			</div>
+			<p className="text-xs text-center mt-1">
+				{t.rich("KOFI.alternative", {
+					Link: (chunks) => (
+						<Link href="https://ko-fi.com/pprmint" target="_blank" className="text-link-external">
+							{chunks}
+						</Link>
+					),
+				})}
+			</p>
+		</div>
+	);
+}
--- src/components/ui/ArtCreditButton.tsx
+++ src/components/ui/ArtCreditButton.tsx
@@ -2,14 +2,14 @@ import { useTranslations } from "next-intl";
 import { PropsWithChildren } from "react";
 import Link from "next/link";
 
-export default function ArtCreditButton(props: PropsWithChildren<{ link: string }>) {
+export default function ArtCreditButton(props: PropsWithChildren<{ link: string; fixToBottom?: boolean }>) {
 	const t = useTranslations("COMMON");
 	return (
 		<Link
 			href={props.link}
 			target="_blank"
 			rel="noopener noreferrer"
-			className="animate-fade-in absolute bottom-36 md:bottom-12 left-6 md:left-auto md:right-12"
+			className={`animate-fade-in absolute ${props.fixToBottom ? "bottom-6 md:bottom-12 right-6 md:right-12" : "bottom-36 md:bottom-12 left-6 md:left-auto md:right-12"}`}
 			style={{ animationDelay: "0.3s", animationFillMode: "backwards" }}
 		>
 			<button className="inline-flex p-0.5 items-center gap-1 text-neutral-50 text-sm px-3 py-1 hover:bg-neutral-50/10 active:opacity-75 rounded-full border border-neutral-50/10 backdrop-blur-sm duration-100 active:duration-75">
