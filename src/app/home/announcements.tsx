24f2a5f1a494d43402d6bc1a77096e9acafe1482
pprmint <git@pprmint.de>
2025-01-11 00:29:00 +0100

    Change design of announcement section more.

--- src/app/home/announcements.tsx
+++ src/app/home/announcements.tsx
@@ -9,34 +9,40 @@ import ChevronLeft from "src/icons/ChevronLeft";
 import { Announcements as AnnouncementsType } from "src/types/announcement";
 import * as m from "motion/react-client";
 import { AnimatePresence } from "motion/react";
+import { useTranslations } from "next-intl";
 
 export default function Announcements({ data }: { data: AnnouncementsType }) {
+	const t = useTranslations("HOME");
 	const [current, setCurrent] = useState(0);
 	const [direction, setDirection] = useState(1);
 
 	const handleNext = () => {
 		setDirection(1);
 		setTimeout(() => {
-			setCurrent((prev) => prev - 1);
+			setCurrent((prev) => (prev - 1 + data.data.length) % data.data.length);
 		}, 1);
 	};
 
 	const handlePrevious = () => {
 		setDirection(-1);
 		setTimeout(() => {
-			setCurrent((prev) => prev + 1);
+			setCurrent((prev) => (prev + 1) % data.data.length);
 		}, 1);
 	};
 
 	return (
-		<section className="w-full max-w-8xl px-6 md:px-9 lg:px-12 xl:px-20 mx-auto">
+		<section className="relative w-full max-w-8xl px-6 md:px-9 lg:px-12 xl:px-20 mx-auto overflow-clip">
+			<h2 className="absolute bottom-12 lg:bottom-24 left-0 text-[10rem] lg:text-[21rem] text-neutral-50/5 -z-10 italic">{t("Content.News.heading")}</h2>
 			<AnimatePresence mode="wait">
-				<m.div key={data.data[current].id} className="grid grid-cols-2 border-x border-neutral-50/5 items-center">
-					<div className="flex col-span-2 lg:col-span-1 flex-col justify-center border-r border-neutral-50/5 h-full w-full lg:py-20 backdrop-blur bg-neutral-950/25">
+				<m.div
+					key={data.data[current].id}
+					className="grid grid-cols-2 border-x border-neutral-50/5 items-center py-20 lg:py-0"
+				>
+					<div className="flex col-span-2 lg:col-span-1 flex-col justify-center lg:border-r border-neutral-50/5 h-full w-full lg:py-40 backdrop-blur bg-neutral-950/25">
 						<div className="pt-4 border-y border-neutral-50/5">
 							<m.h2
 								initial={{ opacity: 0 }}
-								animate={{ opacity: 1, transition: { duration: 0.2 } }}
+								animate={{ opacity: 1, transition: { duration: 0.3 } }}
 								exit={{
 									opacity: 0,
 									transition: { duration: 0.1, ease: "linear" },
@@ -47,7 +53,7 @@ export default function Announcements({ data }: { data: AnnouncementsType }) {
 						</div>
 						<m.p
 							initial={{ opacity: 0 }}
-							animate={{ opacity: 1, transition: { duration: 0.2, delay: 0.05 } }}
+							animate={{ opacity: 1, transition: { duration: 0.3, delay: 0.1 } }}
 							exit={{
 								opacity: 0,
 								transition: { duration: 0.1, ease: "linear" },
@@ -56,10 +62,10 @@ export default function Announcements({ data }: { data: AnnouncementsType }) {
 						>
 							{data.data[current].description}
 						</m.p>
-						<div className="flex h-12 border-y border-neutral-50/5">
+						<div className="flex h-12 border-t lg:border-b border-neutral-50/5">
 							<m.div
 								initial={{ opacity: 0 }}
-								animate={{ opacity: 1, transition: { duration: 0.2, delay: 0.1 } }}
+								animate={{ opacity: 1, transition: { duration: 0.3, delay: 0.2 } }}
 								exit={{
 									opacity: 0,
 									transition: { duration: 0.1, ease: "linear" },
@@ -68,9 +74,14 @@ export default function Announcements({ data }: { data: AnnouncementsType }) {
 							>
 								{data.data[current].link ? (
 									data.data[current].link.startsWith("/") ? (
-										<Link href={data.data[current].link} className="group size-full font-mono hover:bg-neutral-50">
+										<Link
+											href={data.data[current].link}
+											className="group size-full font-mono hover:bg-neutral-50"
+										>
 											<div className="inline-flex h-full items-center gap-3 group-hover:px-4 duration-200 ease-out">
-												<span className="group-hover:text-neutral-950">{data.data[current].linkText}</span>
+												<span className="group-hover:text-neutral-950">
+													{data.data[current].linkText}
+												</span>
 												<div className="relative size-5 overflow-clip group-hover:text-neutral-950">
 													<ArrowRight
 														width={20}
@@ -93,7 +104,9 @@ export default function Announcements({ data }: { data: AnnouncementsType }) {
 											className="group size-full font-mono hover:bg-neutral-50"
 										>
 											<div className="inline-flex h-full items-center gap-3 group-hover:px-4 duration-200 ease-out">
-												<span className="group-hover:text-neutral-950">{data.data[current].linkText}</span>
+												<span className="group-hover:text-neutral-950">
+													{data.data[current].linkText}
+												</span>
 												<div className="relative size-5 overflow-clip group-hover:text-neutral-950">
 													<ArrowUpRight
 														width={20}
@@ -113,74 +126,120 @@ export default function Announcements({ data }: { data: AnnouncementsType }) {
 							</m.div>
 							<div className="relative flex ml-auto border-l border-neutral-50/5 divide-x divide-neutral-50/5">
 								<button
-									disabled={current === 0}
 									className="relative group w-12 h-full text-neutral-50 hover:text-neutral-950 disabled:text-neutral-500 hover:bg-neutral-50 disabled:pointer-events-none overflow-hidden"
 									onClick={handleNext}
 								>
 									<m.div
 										initial={{ x: "100%" }}
-										exit={{ x: direction > 0 ? "0%" : "100%", transition: { type: "spring", duration: 0.2, bounce: 0 } }}
+										exit={{
+											x: direction > 0 ? "0%" : "100%",
+											transition: { type: "spring", duration: 0.2, bounce: 0 },
+										}}
 										className="absolute top-0 inset-0 flex items-center justify-center"
 									>
 										<ChevronLeft />
 									</m.div>
 									<m.div
-										exit={{ x: direction > 0 ? "-100%" : "0%", transition: { type: "spring", duration: 0.2, bounce: 0 } }}
+										exit={{
+											x: direction > 0 ? "-100%" : "0%",
+											transition: { type: "spring", duration: 0.2, bounce: 0 },
+										}}
 										className="absolute top-0 inset-0 flex items-center justify-center"
 									>
 										<ChevronLeft />
 									</m.div>
 								</button>
 								<button
-									disabled={current === data.data.length - 1}
 									className="relative group w-12 h-full text-neutral-50 hover:text-neutral-950 disabled:text-neutral-500 hover:bg-neutral-50 disabled:pointer-events-none overflow-hidden"
 									onClick={handlePrevious}
 								>
 									<m.div
 										initial={{ x: "-100%" }}
-										exit={{ x: direction < 0 ? "0%" : "-100%", transition: { type: "spring", duration: 0.2, bounce: 0 } }}
+										exit={{
+											x: direction < 0 ? "0%" : "-100%",
+											transition: { type: "spring", duration: 0.2, bounce: 0 },
+										}}
 										className="absolute top-0 inset-0 flex items-center justify-center"
 									>
 										<ChevronRight />
 									</m.div>
 									<m.div
-										exit={{ x: direction < 0 ? "100%" : "0%", transition: { type: "spring", duration: 0.2, bounce: 0 } }}
+										exit={{
+											x: direction < 0 ? "100%" : "0%",
+											transition: { type: "spring", duration: 0.2, bounce: 0 },
+										}}
 										className="absolute top-0 inset-0 flex items-center justify-center"
 									>
 										<ChevronRight />
 									</m.div>
 								</button>
 								<m.div
-									className="absolute -bottom-px bg-neutral-50 h-px"
-									style={{ width: 100 / data.data.length, left: `${current * (100 / data.data.length)}%` }}
+									className="absolute bottom-0 lg:-bottom-px bg-neutral-50 h-px"
+									style={{
+										width: `${100 / data.data.length}%`,
+										left: `${current * (100 / data.data.length)}%`,
+									}}
+									animate={{
+										left: `${current * (100 / data.data.length)}%`,
+										transition: { type: "spring", duration: 0.2, bounce: 0 },
+									}}
 									exit={{
-										left: `calc(${current * (100 / data.data.length)}% ${direction > 0 ? "-" : "+"} ${100 / data.data.length}%)`,
+										left:
+											direction < 0
+												? `${((current + 1) % data.data.length) * (100 / data.data.length)}%`
+												: `${((current - 1 + data.data.length) % data.data.length) * (100 / data.data.length)}%`,
 										transition: { type: "spring", duration: 0.2, bounce: 0 },
 									}}
 								/>
 							</div>
 						</div>
 					</div>
-					<m.div
-						initial={{ opacity: 0 }}
-						animate={{ opacity: 1, transition: { type: "spring", bounce: 0, duration: 0.5 } }}
-						exit={{
-							opacity: 0,
-							transition: { duration: 0.1, ease: "linear" },
-						}}
-						className="col-span-2 lg:col-span-1 relative lg:h-full lg:py-20 backdrop-blur bg-neutral-950/25 lg:backdrop-blur-none lg:bg-transparent"
-					>
+					<div className="col-span-2 lg:col-span-1 relative lg:h-full lg:py-40 backdrop-blur bg-neutral-950/25 lg:backdrop-blur-none lg:bg-transparent">
 						<div className="bg-neutral-950/25 border-y border-neutral-50/5">
-							<FadingImage
-								src={`https://static.pprmint.de${data.data[current].media.url}`}
-								alt={data.data[current].media.alternativeText || ""}
-								quality={90}
-								width={data.data[current].media.width}
-								height={data.data[current].media.height}
-								className="w-full"
-							/>
+							<m.div
+								initial={{
+									clipPath:
+										direction < 0
+											? "polygon(0% 0%, 0% 0%, 0% 100%, 0% 100%)"
+											: "polygon(100% 0%, 100% 0%, 100% 100%, 100% 100%)",
+								}}
+								animate={{
+									clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+									transition: { type: "spring", duration: 0.5, bounce: 0 },
+								}}
+								exit={{
+									clipPath:
+										direction < 0
+											? "polygon(100% 0%, 100% 0%, 100% 100%, 100% 100%)"
+											: "polygon(0% 0%, 0% 0%, 0% 100%, 0% 100%)",
+									transition: { ease: "easeIn", duration: 0.2 },
+								}}
+							>
+								<m.div
+									initial={{
+										x: direction < 0 ? -40 : 40,
+									}}
+									animate={{
+										x: 0,
+										transition: { type: "spring", duration: 0.5, bounce: 0 },
+									}}
+									exit={{
+										x: direction < 0 ? 20 : -20,
+										transition: { ease: "easeIn", duration: 0.2 },
+									}}
+								>
+									<FadingImage
+										src={`https://static.pprmint.de${data.data[current].media.url}`}
+										alt={data.data[current].media.alternativeText || ""}
+										quality={90}
+										width={data.data[current].media.width}
+										height={data.data[current].media.height}
+										className="w-full"
+									/>
+								</m.div>
+							</m.div>
 						</div>
-					</m.div>
+					</div>
 				</m.div>
 			</AnimatePresence>
 		</section>
