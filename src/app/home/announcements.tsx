d47bc25089b22c58f36cce52756a6f35c7f52755
pprmint <git@pprmint.de>
2025-01-12 01:26:23 +0100

    Button gradients and other style tweaks.

--- src/app/home/announcements.tsx
+++ src/app/home/announcements.tsx
@@ -32,7 +32,7 @@ export default function Announcements({ data }: { data: AnnouncementsType }) {
 
 	return (
 		<section className="relative w-full max-w-8xl px-6 md:px-9 lg:px-12 xl:px-20 mx-auto overflow-clip">
-			<h2 className="absolute bottom-12 lg:bottom-24 left-0 text-[10rem] lg:text-[20rem] text-neutral-50/5 -z-10 font-serif font-extralight font-ultra-condensed italic">
+			<h2 className="absolute bottom-12 lg:bottom-24 left-0 text-[10rem] lg:text-[21rem] text-neutral-50/5 -z-10 font-serif font-extralight font-ultra-condensed italic">
 				{t("Content.News.heading")}
 			</h2>
 			<AnimatePresence mode="wait">
@@ -41,17 +41,18 @@ export default function Announcements({ data }: { data: AnnouncementsType }) {
 					className="grid grid-cols-2 border-x border-neutral-50/5 items-center py-20 lg:py-0"
 				>
 					<div className="flex col-span-2 lg:col-span-1 flex-col justify-center lg:border-r border-neutral-50/5 h-full w-full lg:py-40 backdrop-blur bg-neutral-950/25">
-						<div className="pt-3 border-y border-neutral-50/5">
-							<m.h2
+						<div className="pt-2 border-y border-neutral-50/5">
+							<m.h3
 								initial={{ opacity: 0 }}
 								animate={{ opacity: 1, transition: { duration: 0.3 } }}
 								exit={{
 									opacity: 0,
 									transition: { duration: 0.1, ease: "linear" },
 								}}
+								className="text-3xl md:text-4xl lg:text-5xl pb-3"
 							>
 								{data.data[current].title}
-							</m.h2>
+							</m.h3>
 						</div>
 						<m.p
 							initial={{ opacity: 0 }}
@@ -79,7 +80,7 @@ export default function Announcements({ data }: { data: AnnouncementsType }) {
 										href={data.data[current].link}
 										target={data.data[current].link.startsWith("/") ? "_self" : "_blank"}
 										rel="noopener noreferrer"
-										className="group size-full hover:bg-neutral-50"
+										className="group size-full hover:bg-gradient-to-b light:hover:bg-gradient-to-t from-neutral-50 to-neutral-100 hover:shadow-md active:shadow-none active:duration-75"
 									>
 										<div className="inline-flex h-full items-center gap-3 group-hover:px-4 duration-200 ease-out">
 											<span className="group-hover:text-neutral-950 group-hover:font-bold duration-0">
@@ -90,12 +91,12 @@ export default function Announcements({ data }: { data: AnnouncementsType }) {
 													<ArrowRight
 														width={20}
 														height={20}
-														className="absolute group-hover:translate-x-full group-hover:duration-300 ease-out-quint"
+														className="absolute"
 													/>
 													<ArrowRight
 														width={20}
 														height={20}
-														className="text-neutral-950 absolute -translate-x-full group-hover:translate-x-0 group-hover:duration-300 ease-out-quint"
+														className="text-neutral-950 stroke-current stroke-[1.5px] absolute -translate-x-full group-hover:translate-x-0 group-hover:duration-300 ease-out-quint"
 													/>
 												</div>
 											) : (
@@ -103,12 +104,12 @@ export default function Announcements({ data }: { data: AnnouncementsType }) {
 													<ArrowUpRight
 														width={20}
 														height={20}
-														className="absolute group-hover:translate-x-full group-hover:-translate-y-full group-hover:duration-300 ease-out-quint"
+														className="absolute"
 													/>
 													<ArrowUpRight
 														width={20}
 														height={20}
-														className="text-neutral-950 absolute -translate-x-full translate-y-full group-hover:translate-x-0 group-hover:translate-y-0 group-hover:duration-300 ease-out-quint"
+														className="text-neutral-950 stroke-current stroke-[1.5px] absolute -translate-x-full translate-y-full group-hover:translate-x-0 group-hover:translate-y-0 group-hover:duration-300 ease-out-quint"
 													/>
 												</div>
 											)}
@@ -118,51 +119,51 @@ export default function Announcements({ data }: { data: AnnouncementsType }) {
 							</m.div>
 							<div className="relative flex ml-auto border-l border-neutral-50/5 divide-x divide-neutral-50/5">
 								<button
-									className="relative group w-12 h-full text-neutral-50 hover:text-neutral-950 disabled:text-neutral-500 hover:bg-neutral-50 disabled:pointer-events-none overflow-hidden"
+									className="relative group w-12 h-full text-neutral-50 hover:text-neutral-950 disabled:text-neutral-500 hover:bg-gradient-to-b light:hover:bg-gradient-to-t from-neutral-50 to-neutral-100 hover:shadow-md active:shadow-none active:duration-75 overflow-hidden"
 									onClick={handleNext}
 								>
 									<m.div
 										initial={{ x: "100%" }}
 										exit={{
 											x: direction > 0 ? "0%" : "100%",
-											transition: { type: "spring", duration: 0.2, bounce: 0 },
+											transition: { ease: "easeOut", duration: 0.2 },
 										}}
 										className="absolute top-0 inset-0 flex items-center justify-center"
 									>
-										<ChevronLeft />
+										<ChevronLeft className="group-hover:stroke-current group-hover:stroke-[1.8]" />
 									</m.div>
 									<m.div
 										exit={{
 											x: direction > 0 ? "-100%" : "0%",
-											transition: { type: "spring", duration: 0.2, bounce: 0 },
+											transition: { ease: "easeOut", duration: 0.2 },
 										}}
 										className="absolute top-0 inset-0 flex items-center justify-center"
 									>
-										<ChevronLeft />
+										<ChevronLeft className="group-hover:stroke-current group-hover:stroke-[1.8]" />
 									</m.div>
 								</button>
 								<button
-									className="relative group w-12 h-full text-neutral-50 hover:text-neutral-950 disabled:text-neutral-500 hover:bg-neutral-50 disabled:pointer-events-none overflow-hidden"
+									className="relative group w-12 h-full text-neutral-50 hover:text-neutral-950 disabled:text-neutral-500 hover:bg-gradient-to-b light:hover:bg-gradient-to-t from-neutral-50 to-neutral-100 hover:shadow-md active:shadow-none active:duration-75 overflow-hidden"
 									onClick={handlePrevious}
 								>
 									<m.div
 										initial={{ x: "-100%" }}
 										exit={{
 											x: direction < 0 ? "0%" : "-100%",
-											transition: { type: "spring", duration: 0.2, bounce: 0 },
+											transition: { ease: "easeOut", duration: 0.2 },
 										}}
 										className="absolute top-0 inset-0 flex items-center justify-center"
 									>
-										<ChevronRight />
+										<ChevronRight className="group-hover:stroke-current group-hover:stroke-[1.8]" />
 									</m.div>
 									<m.div
 										exit={{
 											x: direction < 0 ? "100%" : "0%",
-											transition: { type: "spring", duration: 0.2, bounce: 0 },
+											transition: { ease: "easeOut", duration: 0.2 },
 										}}
 										className="absolute top-0 inset-0 flex items-center justify-center"
 									>
-										<ChevronRight />
+										<ChevronRight className="group-hover:stroke-current group-hover:stroke-[1.8]" />
 									</m.div>
 								</button>
 								<m.div
@@ -187,7 +188,7 @@ export default function Announcements({ data }: { data: AnnouncementsType }) {
 						</div>
 					</div>
 					<div className="col-span-2 lg:col-span-1 relative lg:h-full lg:py-40 backdrop-blur bg-neutral-950/25 lg:backdrop-blur-none lg:bg-transparent">
-						<div className="bg-neutral-950/25 border-y border-neutral-50/5">
+						<div className="bg-neutral-950/25 border-y border-neutral-50/5 aspect-video">
 							<m.div
 								initial={{
 									clipPath:
