fb21ae50c1d5bf28d46669978a4d172db813630c
pprmint <git@pprmint.art>
2025-01-13 16:17:50 +0100

    Redesign Mina section.

--- src/app/home/announcements.tsx
+++ src/app/home/announcements.tsx
@@ -32,11 +32,11 @@ export default function Announcements({ data }: { data: AnnouncementsType }) {
 
 	return (
 		<section className="relative w-full max-w-8xl px-6 md:px-9 lg:px-12 xl:px-20 mx-auto overflow-clip">
-			<h2 className="absolute bottom-12 lg:bottom-24 left-0 text-[10rem] lg:text-[21rem] text-neutral-50/5 -z-10 font-serif font-extralight font-ultra-condensed italic">
+			<h2 className="absolute -bottom-16 left-0 text-[10rem] lg:text-[21rem] text-neutral-50/5 -z-10 font-serif font-extralight font-ultra-condensed italic">
 				{t("Content.News.heading")}
 			</h2>
-			<div className="grid grid-cols-2 border-x border-neutral-50/5 items-center py-20 lg:py-0">
-				<div className="order-2 lg:order-1 flex col-span-2 lg:col-span-1 flex-col justify-center lg:border-r border-neutral-50/5 h-full w-full lg:py-40 backdrop-blur bg-neutral-950/25">
+			<div className="grid grid-cols-2 border-x border-neutral-50/5 items-center pt-20 lg:pt-0">
+				<div className="order-2 lg:order-1 flex col-span-2 lg:col-span-1 flex-col justify-center lg:border-r border-neutral-50/5 h-full w-full lg:pt-40 backdrop-blur bg-neutral-950/25">
 					<AnimatePresence mode="wait">
 						<m.div key={data.data[current].id} className="pt-2 lg:border-t border-neutral-50/5">
 							<m.h3
@@ -187,7 +187,7 @@ export default function Announcements({ data }: { data: AnnouncementsType }) {
 						</div>
 					</div>
 				</div>
-				<div className="order-1 lg:order-2 col-span-2 lg:col-span-1 relative lg:h-full lg:py-40 backdrop-blur bg-neutral-950/25 lg:backdrop-blur-none lg:bg-transparent">
+				<div className="order-1 lg:order-2 col-span-2 lg:col-span-1 relative lg:h-full lg:pt-40 backdrop-blur bg-neutral-950/25 lg:backdrop-blur-none lg:bg-transparent">
 					<div className="bg-neutral-950/25 border-y border-neutral-50/5 aspect-video">
 						<AnimatePresence>
 							<m.div
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,79 +25,58 @@ export default async function Page() {
 			<main>
 				<Announcements data={announcements} />
 				{MinaArt.data && (
-					<section className="w-full max-w-8xl px-6 md:px-9 lg:px-12 xl:px-20 mx-auto overflow-clip">
-						<div className="flex flex-col gap-9 md:flex-row items-center max-w-7xl h-full mx-auto">
-							<div className="flex flex-col w-full">
-								<h2>{t("Content.Mina.heading")}</h2>
-								<p>{t("Content.Mina.text1")}</p>
-								<p className="mb-6">
-									{t.rich("Content.Mina.text2", {
-										artist: MinaArt.data[0].artist.name,
-										link: (chunks) =>
-											MinaArt.data[0].artist.creditUrl ? (
-												<Link href={MinaArt.data[0].artist.creditUrl} className="text-link-external">
-													{chunks}
-												</Link>
-											) : (
-												<span className="font-medium font-neutal-50">{chunks}</span>
-											),
-									})}
-									<br />
-									<span className="text-xs opacity-50">
-										{t.rich("Content.Mina.text3", {
-											i: (chunks) => <i>{chunks}</i>,
-										})}
-									</span>
-								</p>
-								<Link href="/mina" className="w-max">
-									<Button tabIndex={-1} color="green">
-										{t("Content.Mina.button")}
-										<HeartFilled />
-									</Button>
-								</Link>
-							</div>
-							<Link
-								href="/mina#gallery"
-								className="relative group my-12 w-4/5 max-w-fit max-h-4/5 rotate-3 hover:rotate-0 hover:scale-[1.02] active:scale-[0.99] active:brightness-90 duration-400 active:duration-75 ease-out-back active:ease-out rounded-xl outline outline-1 -outline-offset-1 outline-white/10 light:outline-black/10"
-							>
+					<section className="relative w-full max-w-8xl px-6 md:px-9 lg:px-12 xl:px-20 mx-auto">
+						<div className="w-full border-x border-neutral-50/5 pt-64">
+							<div className="relative">
+								<div aria-hidden className="-z-10 absolute italic leading-none text-[10vw] tracking-tighter text-neutral-500">
+									{t("Content.Mina.heading1")}
+								</div>
 								<FadingImage
 									src={`https://static.pprmint.de${MinaArt.data[0].artwork[0].url}`}
 									alt={MinaArt.data[0].artwork[0].alternativeText || ""}
-									width={MinaArt.data[0].artwork[0].width}
-									height={MinaArt.data[0].artwork[0].height}
-									className="rounded-xl"
+									width={MinaArt.data[0].artwork[0].width || 0}
+									height={MinaArt.data[0].artwork[0].height || 0}
+									className="w-auto h-auto max-h-screen mx-auto pt-32 pb-20"
 								/>
 								<div
-									className="absolute inset-0 overflow-clip blur-md rounded-xl"
-									style={{
-										maskImage: `url(https://static.pprmint.de${MinaArt.data[0].artwork[0].url})`,
-										maskRepeat: "no-repeat",
-										maskSize: "100%",
-									}}
+									aria-hidden
+									className="absolute bottom-0 right-0 text-right italic leading-none text-[10vw] font-serif font-ultra-condensed text-neutral-500"
 								>
-									<div className="absolute -left-[225%] lg:-left-full group-hover:left-[150%] top-0 bottom-0 skew-x-[30deg] w-64 group-hover:w-0 bg-white/25 duration-0 group-hover:duration-1000 ease-out-quint" />
+									{t("Content.Mina.heading2")}
 								</div>
-								<Sparkle
-									className="absolute -top-3.5 -right-3.5 group-hover:animate-lucide-sparkle opacity-0"
-									fill="#fff"
-									stroke="none"
-									size={36}
-								/>
-							</Link>
+							</div>
+							<div className="flex flex-col grow">
+								<h2 className="sr-only">{t("Content.Mina.heading1") + " " + t("Content.Mina.heading2")}</h2>
+								<div className="flex flex-col w-full xl:text-xl 2xl:text-2xl">
+									<p>{t("Content.Mina.text1")}</p>
+									<p className="mb-6">
+										{t.rich("Content.Mina.text2", {
+											artist: MinaArt.data[0].artist.name,
+											link: (chunks) =>
+												MinaArt.data[0].artist.creditUrl ? (
+													<Link href={MinaArt.data[0].artist.creditUrl} className="text-link-external">
+														{chunks}
+													</Link>
+												) : (
+													<span className="font-medium font-neutal-50">{chunks}</span>
+												),
+										})}
+										<br />
+										<span className="text-xs opacity-50">
+											{t.rich("Content.Mina.text3", {
+												i: (chunks) => <i>{chunks}</i>,
+											})}
+										</span>
+									</p>
+									<Link href="/mina" className="w-max inline-flex gap-3 items-center hover:bg-neutral-50 hover:text-neutral-950 py-2 hover:px-4 duration-200 ease-out">
+										{t("Content.Mina.button")}
+										<HeartFilled />
+									</Link>
+								</div>
+							</div>
 						</div>
 					</section>
 				)}
-				<section className="relative my-48 max-w-7xl px-6 md:px-9 2xl:px-0 mx-auto">
-					<h2 className="font-extralight text-3xl md:text-4xl lg:text-5xl xl:text-6xl">{t("Content.Contact.questions")}</h2>
-					<Link href="/contact" className="group peer">
-						<h3 className="text-4xl md:text-5xl lg:text-6xl xl:text-7xl group inline-flex text-nowrap gap-3 duration-400 ease-out-expo font-extralight group-hover:font-bold group-active:scale-[0.97]">
-							{t("Content.Contact.answers")}
-							<ArrowRight className="size-[30px] md:size-[45px] xl:size-[60px] stroke-0 group-hover:stroke-1 fill-green stroke-green duration-400 ease-out-expo mt-2.5 lg:mt-4" />
-						</h3>
-					</Link>
-					<Envelope className="size-96 absolute opacity-0 peer-hover:opacity-100 text-elevate right-[10%] peer-hover:right-0 top-1/2 -translate-y-1/2 peer-hover:-rotate-6 duration-500 ease-in-out peer-hover:ease-out-expo pointer-events-none -z-10" />
-				</section>
-				<p className="px-6 md:px-9 text-center text-xs">{t("Content.Contact.pronunciation")}</p>
 			</main>
 		</>
 	);
