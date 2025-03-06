683451d9ec95db3f8fdbcc43b52fc0ee2128ed55
Nicklas <git@pprmint.de>
2025-03-06 08:19:53 +0000

    Redesign loading animation.

---
+++ src/components/loading/GlowingSpinner.tsx
@@ -0,0 +1,30 @@
+export default function GlowingSpinner() {
+	const segments = 5;
+	return (
+		<div className="size-16 -rotate-90">
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+				{[...Array(segments)].map((_, index) => (
+					<g key={index} style={{ transform: `rotate(${index * -1}deg)`, transformOrigin: "center" }}>
+						<ellipse
+							className={`animate-spin fill-transparent stroke-green origin-center`}
+							style={{
+								animationTimingFunction: "cubic-bezier(0.7, 0.25, 0.4, 0.8)",
+								animationDelay: `${index * 0.03}s`,
+								opacity: 1 - index * 0.15,
+							}}
+							strokeLinecap="round"
+							cx="16"
+							cy="16"
+							rx="11"
+							ry="11"
+							strokeWidth={2 - index * 0.225}
+							strokeDasharray={69}
+							strokeDashoffset={73}
+							filter={`blur(${index * 0.3}px)`}
+						/>
+					</g>
+				))}
+			</svg>
+		</div>
+	);
+}
--- src/components/loading/LoadingSpinner.tsx
+++ src/components/loading/LoadingSpinner.tsx
@@ -1,30 +1,35 @@
+import * as m from "motion/react-m"
+
 export default function LoadingSpinner() {
-	const segments = 5;
+	const transition = {
+		duration: 1.5,
+		ease: [0.4, 0, 0, 1],
+		times: [0, 0.5, 1],
+		repeat: Infinity,
+
+	}
 	return (
-		<div className="size-16 -rotate-90">
-			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
-				{[...Array(segments)].map((_, index) => (
-					<g key={index} style={{ transform: `rotate(${index * -1}deg)`, transformOrigin: "center" }}>
-						<ellipse
-							className={`animate-spin fill-transparent stroke-green origin-center`}
-							style={{
-								animationTimingFunction: "cubic-bezier(0.7, 0.25, 0.4, 0.8)",
-								animationDelay: `${index * 0.03}s`,
-								opacity: 1 - index * 0.15,
-							}}
-							strokeLinecap="round"
-							cx="16"
-							cy="16"
-							rx="11"
-							ry="11"
-							strokeWidth={2 - index * 0.225}
-							strokeDasharray={69}
-							strokeDashoffset={73}
-							filter={`blur(${index * 0.3}px)`}
-						/>
-					</g>
-				))}
-			</svg>
+		<div className="relative size-8">
+			<m.div
+				animate={{
+					x: ["0%", "100%", "100%", "0%", "0%"],
+					y: ["0%", "0%", "100%", "100%", "0%"],
+					skewX: [0, -6, 0, 0, 0, -6, 0, 0, 0],
+					skewY: [0, 0, 0, 6, 0, 0, 0, 6, 0]
+				}}
+				transition={transition}
+				className="absolute size-4 bg-green"
+			/>
+			<m.div
+				animate={{
+					x: ["100%", "0%", "0%", "100%", "100%"],
+					y: ["100%", "100%", "0%", "0%", "100%"],
+					skewX: [0, -6, 0, 0, 0, -6, 0, 0, 0],
+					skewY: [0, 0, 0, 6, 0, 0, 0, 6, 0]
+				}}
+				transition={transition}
+				className="absolute translate-x-full translate-y-full size-4 bg-green"
+			/>
 		</div>
 	);
 }
