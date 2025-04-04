13a975c4d81b67f066f988c312b2f521a79712fe
pprmint <git@pprmint.art>
2025-04-04 13:06:19 +0200

    Remove unused import.

--- src/collections/Projects.ts
+++ src/collections/Projects.ts
@@ -1,7 +1,6 @@
 import type { CollectionConfig } from "payload";
 import { anyone } from "../access/anyone";
 import { authenticated } from "../access/authenticated";
-import { FixedToolbarFeature, InlineToolbarFeature, lexicalEditor } from "@payloadcms/richtext-lexical";
 
 export const Projects: CollectionConfig = {
 	slug: "projects",
