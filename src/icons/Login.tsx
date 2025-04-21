c71674ec84f290ef7ec5e4493a513977d27be23a
pprmint <git@pprmint.de>
2025-04-21 23:48:14 +0200

    Shift by a pixel to center the door element.

--- src/icons/Login.tsx
+++ src/icons/Login.tsx
@@ -1,6 +1,16 @@
 import { SVGAttributes } from "react";
 export default function Login({ ...props }: SVGAttributes<SVGElement>) {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 15 15" fill="currentColor" {...props}><path d="M5 6H4V2.5A1.5 1.5 0 0 1 5.5 1h6A1.5 1.5 0 0 1 13 2.5v10a1.5 1.5 0 0 1-1.5 1.5h-6A1.5 1.5 0 0 1 4 12.5V9h1v3.5a.5.5 0 0 0 .5.5h6a.5.5 0 0 0 .5-.5v-10a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0-.5.5z"/><path d="M8.293 8H2V7h6.293l-2-2h1.414l2.5 2.5-2.5 2.5H6.293z"/></svg>
-  );
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="15"
+			height="15"
+			viewBox="0 0 15 15"
+			fill="currentColor"
+			{...props}
+		>
+			<path d="M4 6H3V2.5C3 1.67 3.67 1 4.5 1h6c.83 0 1.5.67 1.5 1.5v10c0 .83-.67 1.5-1.5 1.5h-6A1.5 1.5 0 0 1 3 12.5V9h1v3.5c0 .28.22.5.5.5h6a.5.5 0 0 0 .5-.5v-10a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0-.5.5V6Z" />
+			<path d="M7.3 8H1V7h6.3l-2-2h1.4l2.5 2.5L6.7 10H5.3l2-2Z" />
+		</svg>
+	);
 }
--- src/icons/Logout.tsx
+++ src/icons/Logout.tsx
@@ -1,6 +1,16 @@
 import { SVGAttributes } from "react";
 export default function Logout({ ...props }: SVGAttributes<SVGElement>) {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 15 15" fill="currentColor" {...props}><path d="M10 11h1v1.5A1.5 1.5 0 0 1 9.5 14h-6A1.5 1.5 0 0 1 2 12.5v-10A1.5 1.5 0 0 1 3.5 1h6A1.5 1.5 0 0 1 11 2.5V4h-1V2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0-.5.5v10a.5.5 0 0 0 .5.5h6a.5.5 0 0 0 .5-.5z"/><path d="M11.293 8H5V7h6.293l-2-2h1.414l2.5 2.5-2.5 2.5H9.293z"/></svg>
-  );
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="15"
+			height="15"
+			viewBox="0 0 15 15"
+			fill="currentColor"
+			{...props}
+		>
+			<path d="M11 11h1v1.5c0 .83-.67 1.5-1.5 1.5h-6A1.5 1.5 0 0 1 3 12.5v-10C3 1.67 3.67 1 4.5 1h6c.83 0 1.5.67 1.5 1.5V4h-1V2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0-.5.5v10c0 .28.22.5.5.5h6a.5.5 0 0 0 .5-.5V11Z" />
+			<path d="M12.3 8H6V7h6.3l-2-2h1.4l2.5 2.5-2.5 2.5h-1.4l2-2Z" />
+		</svg>
+	);
 }
