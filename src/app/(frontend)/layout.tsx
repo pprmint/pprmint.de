7a7913e672d71254c37654a01dc9783c12b0156d
Nicklas <git@pprmint.de>
2025-05-05 12:00:29 +0200

    ...as an URL.

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -22,7 +22,7 @@ import { TooltipProvider } from "@radix-ui/react-tooltip";
 import TransitionProvider from "./transitionProvider";
 
 export const metadata: Metadata = {
-	metadataBase: process.env.NEXT_PUBLIC_SERVER_URL,
+	metadataBase: new URL(process.env.NEXT_PUBLIC_SERVER_URL),
 	title: {
 		template: `%s • pprmint.`,
 		default: `pprmint.`,
