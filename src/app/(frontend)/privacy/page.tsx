f5ea637f12e7c3d031d69d6aa7e1f10c9774b9f4
pprmint <git@pprmint.de>
2025-05-22 20:15:44 +0200

    Fix some styles on the privacy policy page.

--- src/app/(frontend)/privacy/page.tsx
+++ src/app/(frontend)/privacy/page.tsx
@@ -6,36 +6,39 @@ import Title from "@/components/layout/Title";
 import { getLocale, getTranslations } from "next-intl/server";
 
 export async function generateMetadata() {
-  const t = await getTranslations("PRIVACY");
-  return {
-    title: t("Head.title"),
-    description: t("Head.description"),
-  };
+	const t = await getTranslations("PRIVACY");
+	return {
+		title: t("Head.title"),
+		description: t("Head.description"),
+	};
 }
 
 export default async function Page() {
-  const t = await getTranslations("PRIVACY");
-  const locale = (await getLocale()) as "en" | "de" | "all" | undefined;
+	const t = await getTranslations("PRIVACY");
+	const locale = (await getLocale()) as "en" | "de" | "all" | undefined;
 
-  const payload = await getPayload({ config: configPromise });
-  const privacyPolicy = await payload.findGlobal({
-    slug: "privacyPolicy",
-    locale: locale,
-  });
+	const payload = await getPayload({ config: configPromise });
+	const privacyPolicy = await payload.findGlobal({
+		slug: "privacyPolicy",
+		locale: locale,
+	});
 
-  return (
-    <>
-      <Title title={t("Head.title")} description={t("Head.description")} />
-      <main className="max-w-8xl mx-auto px-6 md:px-9 xl:px-20">
-        <section className="pb-20 md:pb-32 border-x border-black/5 dark:border-white/5">
-          <div className="bg-black/5 dark:bg-white/5 px-6 md:px-9 py-6 md:py-9">
-            <RichText data={privacyPolicy.tldr} />
-          </div>
-        </section>
-        <section className="border-x border-black/5 dark:border-white/5">
-          <RichText data={privacyPolicy.main} />
-        </section>
-      </main>
-    </>
-  );
+	return (
+		<>
+			<Title title={t("Head.title")} description={t("Head.description")} />
+			<main className="max-w-8xl mx-auto px-6 md:px-9 xl:px-20">
+				<section className="pb-20 md:pb-32 border-x border-black/5 dark:border-white/5">
+					<div className="bg-black/5 dark:bg-white/5 px-6 md:px-9 py-6 md:py-9">
+						<RichText data={privacyPolicy.tldr} className="prose-h2:pt-0" />
+					</div>
+				</section>
+				<section className="border-x border-black/5 dark:border-white/5">
+					<RichText data={privacyPolicy.main} />
+				</section>
+				<section className="border-x border-black/5 dark:border-white/5 pt-10 text-center text-xs">
+					<p>{t("Content.lastUpdated", { revisionDate: new Date(privacyPolicy.updatedAt!) })}</p>
+				</section>
+			</main>
+		</>
+	);
 }
--- src/components/richText.tsx
+++ src/components/richText.tsx
@@ -52,9 +52,9 @@ export default function RichText(props: Props) {
 			className={cn(
 				"prose-em:italic",
 				"prose-ul:list-disc prose-ul:list-inside prose-ul:ml-[0.5em] prose-ul:mb-3",
-				"prose-h2:mt-9 prose-h3:mt-9 prose-h4:mt-9 prose-h5:mt-9",
+				"prose-h2:pt-9 prose-h3:pt-9 prose-h4:pt-9 prose-h5:pt-9",
 				"prose-blockquote:font-serif prose-blockquote:text-lg prose-blockquote:text-black dark:prose-blockquote:text-white prose-blockquote:bg-black/5 dark:prose-blockquote:bg-white/5 prose-blockquote:px-3 prose-blockquote:py-2 prose-blockquote:mb-3",
-				"prose-hr:border-black/5 dark:prose-hr:border-white/5 prose-hr:mt-9 prose-a:text-link",
+				"prose-hr:border-black/5 dark:prose-hr:border-white/5 prose-hr:pt-9 prose-a:text-link",
 				"prose-code:bg-black/5 dark:prose-code:bg-white/5 prose-code:leading-3 prose-code:font-mono prose-code:text-[0.95em] prose-code:px-1 prose-code:py-0.5 prose-code:text-neutral-950 dark:prose-code:text-white",
 				className
 			)}
