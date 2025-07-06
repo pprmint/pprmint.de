1d96be4cc8fd1dcc4e5870d3093e2a39c994500b
pprmint <git@pprmint.de>
2025-07-06 21:43:11 +0200

    Fix type error.

--- src/components/loading/LoadingSpinner.tsx
+++ src/components/loading/LoadingSpinner.tsx
@@ -1,13 +1,14 @@
-import * as m from "motion/react-m"
+import { Transition } from "motion/react";
+import * as m from "motion/react-m";
 
 export default function LoadingSpinner() {
-	const transition = {
+	const transition: Transition<any> = {
 		duration: 1.5,
 		ease: [0.4, 0, 0, 1],
 		times: [0, 0.5, 1],
 		repeat: Infinity,
+	};
 
-	}
 	return (
 		<div className="relative size-8">
 			<m.div
@@ -15,7 +16,7 @@ export default function LoadingSpinner() {
 					x: ["0%", "100%", "100%", "0%", "0%"],
 					y: ["0%", "0%", "100%", "100%", "0%"],
 					skewX: [0, -6, 0, 0, 0, -6, 0, 0, 0],
-					skewY: [0, 0, 0, 6, 0, 0, 0, 6, 0]
+					skewY: [0, 0, 0, 6, 0, 0, 0, 6, 0],
 				}}
 				transition={transition}
 				className="absolute size-4 bg-green"
@@ -25,7 +26,7 @@ export default function LoadingSpinner() {
 					x: ["100%", "0%", "0%", "100%", "100%"],
 					y: ["100%", "100%", "0%", "0%", "100%"],
 					skewX: [0, -6, 0, 0, 0, -6, 0, 0, 0],
-					skewY: [0, 0, 0, 6, 0, 0, 0, 6, 0]
+					skewY: [0, 0, 0, 6, 0, 0, 0, 6, 0],
 				}}
 				transition={transition}
 				className="absolute translate-x-full translate-y-full size-4 bg-green"
