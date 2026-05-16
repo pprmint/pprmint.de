f175786c1e178795e6c13459f83b8d19ed32e94e
pprmint <git@pprmint.de>
2026-05-16 19:22:43 +0200

    Less corny text.

--- src/app/(frontend)/mina/filters/nsfwDialog.tsx
+++ src/app/(frontend)/mina/filters/nsfwDialog.tsx
@@ -13,44 +13,25 @@ export default function NsfwDialog({ onAccept }: { onAccept: () => void }) {
 		<Dialog.Portal>
 			<Dialog.Overlay className="bg-white/90 dark:bg-neutral-950/90 data-[state=open]:animate-fade-in data-[state=closed]:animate-fade-out fixed inset-0 z-100" />
 			<Dialog.Content className="fixed z-100 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 max-h-svh md:h-max w-[93vw] max-w-5xl bg-white dark:bg-neutral-950 outline-solid outline-1 outline-black/5 dark:outline-white/5 data-[state=open]:animate-dialog-enter data-[state=closed]:animate-dialog-exit origin-center shadow-2xl">
-				<div className="bg-white sm:flex items-center md:flex-row border-b dark:border-0 border-black/5">
-					<div className="p-6 md:p-9">
-						<Dialog.Title asChild>
-							<h2 className="lg:text-balance dark:text-neutral-950">
-								{t("Content.NSFW.title")}
-								<span className="text-red">.</span>
-							</h2>
-						</Dialog.Title>
-						<p className="md:text-lg dark:text-neutral-950/75">
-							{t.rich("Content.NSFW.subtitle", {
-								i: (chunks) => <span className="italic">{chunks}</span>,
-							})}
-						</p>
-					</div>
-					<FadingImage
-						hideSpinner
-						src="/api/assets/file/kozu_mina_handholding.webp"
-						alt=""
-						width={1029}
-						height={718}
-						quality={100}
-						className="h-32 md:h-64 w-auto ml-auto"
-					/>
-				</div>
+				<Dialog.Title asChild>
+					<h2 className="px-6 md:px-9 mt-6 md:mt-9 md:text-center lg:text-balance">
+						{t("Content.NSFW.title")}
+					</h2>
+				</Dialog.Title>
 				<Dialog.Description asChild>
-					<div className="p-6 md:p-9 md:text-center">
+					<div className="px-6 md:px-9 md:text-center">
 						<p>{t("Content.NSFW.text1")}</p>
 						<p>{t("Content.NSFW.text2")}</p>
 						<p className="font-bold text-red-600 dark:text-red">{t("Content.NSFW.text3")}</p>
 					</div>
 				</Dialog.Description>
-				<div className="grid grid-cols-2 border-t border-black/5 dark:border-white/5 h-12 divide-x divide-black/5 dark:divide-white/5">
+				<div className="grid grid-cols-2 mt-6 md:mt-9 border-t border-black/5 dark:border-white/5 h-12 divide-x divide-black/5 dark:divide-white/5">
 					<Button align="center" size="full" color="red" onClick={onAccept}>
-						{t("Content.NSFW.admitSins")}
+						{t("Content.NSFW.accept")}
 					</Button>
 					<Dialog.Close asChild>
 						<Button align="center" size="full">
-							{t("Content.NSFW.nevermind")}
+							{t("Content.NSFW.cancel")}
 						</Button>
 					</Dialog.Close>
 				</div>
