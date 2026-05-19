80d4bede83d7267b0003a238dbc6f4297ab87b60
pprmint <git@pprmint.de>
2026-05-19 20:49:32 +0200

    There ya go, reused.

---
+++ src/app/(frontend)/fonts/FontSection.tsx
@@ -0,0 +1,132 @@
+"use client";
+import Slider from "@/components/ui/Slider";
+import Tooltip from "@/components/ui/Tooltip";
+import Pilcrow from "@/icons/Pilcrow";
+import TextItalic from "@/icons/TextItalic";
+import TextItalicGerman from "@/icons/TextItalicGerman";
+import { useLocale, useTranslations } from "next-intl";
+import { Link } from "next-transition-router";
+import { NextFontWithVariable } from "next/dist/compiled/@next/font";
+import { PropsWithChildren, useState } from "react";
+
+export default function FontSection(
+	props: PropsWithChildren<{
+		name: string;
+		link: string;
+		font: NextFontWithVariable;
+		variable?: boolean;
+		styles: {
+			tag: string;
+			steps: number[];
+			initial?: number;
+		}[];
+		hasItalic?: boolean;
+	}>,
+) {
+	const t = useTranslations("FONT");
+	const locale = useLocale() as "en" | "de" | "all" | undefined;
+
+	const [linebreak, setLinebreak] = useState(false);
+	const [italic, setItalic] = useState(false);
+	const [size, setSize] = useState([6]);
+
+	const [values, setValues] = useState(props.styles.map((style) => style.initial ?? 0));
+	function updateStyle(index: number, value: number) {
+		setValues((prev) => prev.map((v, i) => (i === index ? value : v)));
+	}
+
+	return (
+		<section className="group border-x border-b first:border-t border-black/5 dark:border-white/5 pb-6 md:pb-9">
+			<div className="flex w-full justify-between pl-6 md:pl-9 mb-3">
+				<Link
+					href={props.link}
+					target={props.link.startsWith("https://") ? "_blank" : "_self"}
+					rel={props.link.startsWith("https://") ? "noopener noreferrer" : ""}
+					className="font-medium hover:text-black dark:hover:text-white duration-75 mt-5 md:mt-8"
+				>
+					{props.name}
+				</Link>
+				<div className="flex h-max opacity-0 group-hover:opacity-100 duration-75 border-b border-l border-black/5 dark:border-white/5">
+					<div className="hidden md:flex">
+						{props.variable &&
+							props.styles.map((style, i) => (
+								<div className="w-48 lg:w-72 xl:w-84 2xl:w-100 border-r border-black/5 dark:border-white/5" key={style.tag}>
+									<Slider
+										label={t(`Axis.${style.tag}`)}
+										value={[values[i]]}
+										onValueChange={([sliderIndex]) => {
+											updateStyle(i, sliderIndex);
+										}}
+										step={1}
+										min={style.steps[0]}
+										max={style.steps[style.steps.length - 1]}
+									/>
+								</div>
+							))}
+						<div className="w-48 lg:w-72 xl:w-84 2xl:w-100 border-r border-black/5 dark:border-white/5">
+							<Slider
+								label={t("size")}
+								value={size}
+								unit="rem"
+								onValueChange={(value) => setSize(value)}
+								step={0.1}
+								min={0.5}
+								max={15}
+							/>
+						</div>
+					</div>
+					{props.hasItalic && (
+						<Tooltip text={t("Style.italic")}>
+							<button
+								className={`size-9 ${italic ? "bg-neutral-950 hover:bg-neutral-900 dark:bg-white dark:hover:bg-neutral-100 text-white dark:text-neutral-950" : "hover:bg-black/5 dark:hover:bg-white/5 text-neutral-950 dark:text-white"} duration-75 cursor-pointer`}
+								onClick={() => setItalic(!italic)}
+							>
+								{locale === "de" ? <TextItalicGerman className="mx-auto" /> : <TextItalic className="mx-auto" />}
+							</button>
+						</Tooltip>
+					)}
+					<Tooltip text={t("lineBreak")}>
+						<button
+							className={`size-9 ${linebreak ? "bg-neutral-950 hover:bg-neutral-900 dark:bg-white dark:hover:bg-neutral-100 text-white dark:text-neutral-950" : "hover:bg-black/5 dark:hover:bg-white/5 text-neutral-950 dark:text-white"} duration-75 cursor-pointer`}
+							onClick={() => setLinebreak(!linebreak)}
+						>
+							<Pilcrow className="mx-auto" />
+						</button>
+					</Tooltip>
+				</div>
+			</div>
+			<div className="relative">
+				<div
+					className={`${props.font.className} min-h-30 px-6 md:px-9 mb-3 text-4xl md:text-6xl text-black dark:text-white outline-0 leading-tight ${linebreak ? "whitespace-normal" : "whitespace-nowrap overflow-x-auto overflow-y-hidden"}`}
+					contentEditable
+					suppressContentEditableWarning
+					spellCheck={false}
+					onChange={() => setLinebreak(!linebreak)}
+					style={{
+						fontStyle: italic ? "italic" : "normal",
+						fontVariationSettings: props.styles.map((style, i) => `'${style.tag}' ${values[i]}`).join(", "),
+						fontSize: `${size}rem`,
+					}}
+				>
+					{props.children}
+				</div>
+				<div className="absolute left-0 inset-y-0 bg-linear-to-r from-white dark:from-neutral-950 w-6 md:w-9" />
+				<div className="absolute right-0 inset-y-0 bg-linear-to-l from-white dark:from-neutral-950 w-6 md:w-9" />
+			</div>
+			<div className="flex gap-6 px-6 md:px-9 text-xs">
+				<div>
+					{t(`Type.${props.variable ? "variable" : "static"}`)}
+					{props.variable && ` (${props.styles.map((style) => t(`Axis.${style.tag}`)).join(", ")})`}
+				</div>
+				<div>
+					{t("styles", {
+						count:
+							props.styles.reduce((total, style) => {
+								return total * style.steps.length;
+							}, 1) * (props.hasItalic ? 2 : 1),
+					})}
+				</div>
+			</div>
+		</section>
+	);
+}
--- src/app/(frontend)/fonts/page.tsx
+++ src/app/(frontend)/fonts/page.tsx
@@ -1,15 +1,12 @@
 import Title from "@/components/layout/Title";
-import { useTranslations } from "next-intl";
 import { getTranslations } from "next-intl/server";
 
 import { MNVaria } from "@public/fonts/MNVaria/MNVaria";
-import Link from "next/link";
 import { MNCelesta } from "@public/fonts/MNCelesta/MNCelesta";
 import { MintBit } from "@public/fonts/Mintbit/MintBit";
-import { PropsWithChildren } from "react";
-import { NextFontWithVariable } from "next/dist/compiled/@next/font";
 import { MintSans } from "@public/fonts/MintSans/MintSans";
 import { MintTriangles } from "@public/fonts/MintTriangles/MintTriangles";
+import FontSection from "./FontSection";
 
 export async function generateMetadata() {
 	const t = await getTranslations("FONTS");
@@ -19,73 +16,8 @@ export async function generateMetadata() {
 	};
 }
 
-export default function Page() {
-	const t = useTranslations("FONTS");
-
-	function FontSection(
-		props: PropsWithChildren<{
-			name: string;
-			link: string;
-			font: NextFontWithVariable;
-			type: "static" | "variable";
-			axes?: {
-				tag: string;
-				range: number[];
-			}[],
-			staticStyles: {
-				tag: string;
-				steps: number[];
-			}[],
-			hasItalic?: boolean;
-		}>,
-	) {
-		return (
-			<section className="border-x border-b first:border-t border-black/5 dark:border-white/5 py-6 md:py-9">
-				<div className="px-6 md:px-9 mb-3">
-					<Link
-						href={props.link}
-						target={props.link.startsWith("https://") ? "_blank" : "_self"}
-						rel={props.link.startsWith("https://") ? "noopener noreferrer" : ""}
-						className="font-medium hover:text-black dark:hover:text-white duration-75"
-					>
-						{props.name}
-					</Link>
-				</div>
-				<div className="relative">
-					<div
-						className={`${props.font.className} px-6 md:px-9 mb-3 text-4xl md:text-6xl lg:text-8xl text-black dark:text-white outline-0 leading-tight whitespace-nowrap overflow-x-auto overflow-y-hidden`}
-						contentEditable
-						suppressContentEditableWarning
-						spellCheck={false}
-					>
-						{props.children}
-					</div>
-					<div className="absolute left-0 inset-y-0 bg-linear-to-r from-white dark:from-neutral-950 w-6 md:w-9" />
-					<div className="absolute right-0 inset-y-0 bg-linear-to-l from-white dark:from-neutral-950 w-6 md:w-9" />
-				</div>
-				<div className="flex gap-6 px-6 md:px-9 text-xs">
-					<div>{t(`Content.FontSection.Type.${props.type}`)}</div>
-					{props.axes && (
-						<div>
-							{props.axes.map((axis) => (
-								<div key={axis.tag}>
-									{t(`Content.FontSection.Axis.${axis.tag}`)} [{axis.range[0]}-{axis.range[1]}]
-								</div>
-							))}
-						</div>
-					)}
-					<div>
-						{t("Content.FontSection.variants", {
-							count: props.staticStyles.reduce((total, axis) => {
-								return total * axis.steps.length;
-							}, 1) * (props.hasItalic ? 2 : 1),
-						})}
-					</div>
-				</div>
-			</section>
-		);
-	}
-
+export default async function Page() {
+	const t = await getTranslations("FONTS");
 	return (
 		<>
 			<Title title={t("Head.title")} description={t("Head.description")} />
@@ -94,60 +26,68 @@ export default function Page() {
 					name="MN Varia"
 					font={MNVaria}
 					link="/fonts/varia"
-					type="variable"
-					axes={[{ tag: "wght", range: [100, 900] }]}
-					staticStyles={[
+					variable
+					styles={[
 						{
 							tag: "wght",
 							steps: [100, 200, 300, 400, 500, 600, 700, 800, 900],
-						}
+							initial: 400
+						},
 					]}
 				>
-					Entzückt durch ihre vielen Zeichenvarianten.
+					MN Varia, née Mina Sans, entzückt mit vielen Zeichenvarianten.
 				</FontSection>
 				<FontSection
 					name="MN Celesta"
 					font={MNCelesta}
 					link="https://github.com/pprmint/MN-Celesta"
-					type="static"
-					staticStyles={[
+					styles={[
 						{
 							tag: "wght",
 							steps: [400],
-						}
+						},
 					]}
 					hasItalic
 				>
 					A fork of the brilliant Instrument Serif.
 				</FontSection>
-				<FontSection name="MintBit" font={MintBit} link="/fonts/mintbit" type="static" staticStyles={[
-					{
-						tag: "wght",
-						steps: [400],
-					}
-				]}>
-					Pixelated goodness.
+				<FontSection
+					name="MintBit"
+					font={MintBit}
+					link="/fonts/mintbit"
+					styles={[
+						{
+							tag: "wght",
+							steps: [400],
+						},
+					]}
+				>
+					Minenhandwerk. Vanille. Geil man.
 				</FontSection>
 				<FontSection
 					name="MintTriangles"
 					font={MintTriangles}
 					link="/fonts/minttriangles"
-					type="static"
-					staticStyles={[
+					styles={[
 						{
 							tag: "wght",
 							steps: [400],
-						}
+						},
 					]}
 				>
 					12:34:56
 				</FontSection>
-				<FontSection name="MintSans" font={MintSans} link="/fonts/mintsans" type="static" staticStyles={[
-					{
-						tag: "wght",
-						steps: [100, 400, 700],
-					}
-				]}>
+				<FontSection
+					name="MintSans"
+					font={MintSans}
+					link="/fonts/mintsans"
+					styles={[
+						{
+							tag: "wght",
+							steps: [100, 400, 700],
+						},
+					]}
+				>
 					Used to be "Butterknife", belongs in the scrapyard.
 				</FontSection>
 			</main>
