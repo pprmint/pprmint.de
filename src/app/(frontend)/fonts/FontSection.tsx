7faa805d92aaaa35300ce2eadd90a15cc2853ffb
pprmint <git@pprmint.de>
2026-05-24 02:14:34 +0200

    Remove line break toggle, hopefully disallow pasted text with formatting. More whitespace changes than actual changes. lmao

--- src/app/(frontend)/fonts/FontSection.tsx
+++ src/app/(frontend)/fonts/FontSection.tsx
@@ -2,7 +2,6 @@
 import Select from "@/components/ui/Select";
 import Slider from "@/components/ui/Slider";
 import Tooltip from "@/components/ui/Tooltip";
-import Pilcrow from "@/icons/Pilcrow";
 import TextItalic from "@/icons/TextItalic";
 import TextItalicGerman from "@/icons/TextItalicGerman";
 import { useLocale, useTranslations } from "next-intl";
@@ -27,7 +26,6 @@ export default function FontSection(
 	const t = useTranslations("FONT");
 	const locale = useLocale() as "en" | "de" | "all" | undefined;
 
-	const [linebreak, setLinebreak] = useState(false);
 	const [italic, setItalic] = useState(false);
 	const [size, setSize] = useState([6]);
 
@@ -47,48 +45,48 @@ export default function FontSection(
 				>
 					{props.name}
 				</Link>
-				<div className="flex h-max opacity-0 group-hover:opacity-100 duration-75 border-b border-l border-black/5 dark:border-white/5">
+				<div className="flex h-max opacity-0 group-hover:opacity-100 duration-75 border-b border-black/5 dark:border-white/5">
 					<div className="hidden md:flex">
 						{props.variable
 							? props.styles.map((style, i) => (
-									<div
-										className="w-48 lg:w-72 xl:w-84 2xl:w-100 border-r border-black/5 dark:border-white/5"
-										key={style.tag}
-									>
-										<Slider
-											label={t(`Axis.${style.tag}`)}
-											value={[values[i]]}
-											onValueChange={([sliderIndex]) => {
-												updateStyle(i, sliderIndex);
-											}}
-											step={1}
-											min={style.steps[0]}
-											max={style.steps[style.steps.length - 1]}
-										/>
-									</div>
-								))
+								<div
+									className="w-48 lg:w-72 xl:w-84 2xl:w-100 border-l border-black/5 dark:border-white/5"
+									key={style.tag}
+								>
+									<Slider
+										label={t(`Axis.${style.tag}`)}
+										value={[values[i]]}
+										onValueChange={([sliderIndex]) => {
+											updateStyle(i, sliderIndex);
+										}}
+										step={1}
+										min={style.steps[0]}
+										max={style.steps[style.steps.length - 1]}
+									/>
+								</div>
+							))
 							: props.styles.reduce((total, style) => {
-									return total * style.steps.length;
-								}, 1) > 1 &&
-								props.styles.map((style, i) => (
-									<div key={style.tag} className="w-42 lg:w-58 border-r border-black/5 dark:border-white/5">
-										<Select
-											label={t(`Axis.${style.tag}`)}
-											selected={{
-												value: String(values[props.styles.findIndex((i) => i.tag === style.tag)]),
-												label: t(`Weight.${values[props.styles.findIndex((i) => i.tag === style.tag)]}`),
-											}}
-											options={style.steps.map((step) => ({
-												value: String(step),
-												label: t(`Weight.${step}`),
-											}))}
-											onValueChange={(value) => {
-												updateStyle(i, parseInt(value));
-											}}
-										/>
-									</div>
-								))}
-						<div className="w-48 lg:w-72 xl:w-84 2xl:w-100 border-r border-black/5 dark:border-white/5">
+								return total * style.steps.length;
+							}, 1) > 1 &&
+							props.styles.map((style, i) => (
+								<div key={style.tag} className="w-42 lg:w-58 border-l border-black/5 dark:border-white/5">
+									<Select
+										label={t(`Axis.${style.tag}`)}
+										selected={{
+											value: String(values[props.styles.findIndex((i) => i.tag === style.tag)]),
+											label: t(`Weight.${values[props.styles.findIndex((i) => i.tag === style.tag)]}`),
+										}}
+										options={style.steps.map((step) => ({
+											value: String(step),
+											label: t(`Weight.${step}`),
+										}))}
+										onValueChange={(value) => {
+											updateStyle(i, parseInt(value));
+										}}
+									/>
+								</div>
+							))}
+						<div className="w-48 lg:w-72 xl:w-84 2xl:w-100 border-l border-black/5 dark:border-white/5">
 							<Slider
 								label={t("size")}
 								value={size}
@@ -110,35 +108,26 @@ export default function FontSection(
 							</button>
 						</Tooltip>
 					)}
-					<Tooltip text={t("lineBreak")}>
-						<button
-							className={`size-9 ${linebreak ? "bg-neutral-950 hover:bg-neutral-900 dark:bg-white dark:hover:bg-neutral-100 text-white dark:text-neutral-950" : "hover:bg-black/5 dark:hover:bg-white/5 text-neutral-950 dark:text-white"} duration-75 cursor-pointer`}
-							onClick={() => setLinebreak(!linebreak)}
-						>
-							<Pilcrow className="mx-auto" />
-						</button>
-					</Tooltip>
 				</div>
 			</div>
 			<div className="relative">
 				<div
-					className={`${props.font.className} min-h-30 px-6 md:px-9 mb-3 text-4xl md:text-6xl text-black dark:text-white outline-0 leading-tight ${linebreak ? "whitespace-normal" : "whitespace-nowrap overflow-x-auto overflow-y-hidden"}`}
-					contentEditable
-					suppressContentEditableWarning
+					className={`${props.font.className} min-h-30 px-6 md:px-9 mb-3 text-4xl md:text-6xl text-black dark:text-white outline-0 leading-tight`}
 					spellCheck={false}
-					onChange={() => setLinebreak(!linebreak)}
+					contentEditable="plaintext-only"
+					suppressContentEditableWarning
 					style={
 						props.variable
 							? {
-									fontStyle: italic ? "italic" : "normal",
-									fontVariationSettings: props.styles.map((style, i) => `'${style.tag}' ${values[i]}`).join(", "),
-									fontSize: `${size}rem`,
-								}
+								fontStyle: italic ? "italic" : "normal",
+								fontVariationSettings: props.styles.map((style, i) => `'${style.tag}' ${values[i]}`).join(", "),
+								fontSize: `${size}rem`,
+							}
 							: {
-									fontStyle: italic ? "italic" : "normal",
-									fontSize: `${size}rem`,
-									fontWeight: `${values[props.styles.findIndex((i) => i.tag === "wght")]}`,
-								}
+								fontStyle: italic ? "italic" : "normal",
+								fontSize: `${size}rem`,
+								fontWeight: `${values[props.styles.findIndex((i) => i.tag === "wght")]}`,
+							}
 					}
 				>
 					{props.children}
