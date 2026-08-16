965649530e144b55a0e6febe2e3aaf7a79ba5cad
pprmint <git@pprmint.de>
2026-08-16 17:42:52 +0200

    Scale arrow down on smaller viewports and get rid of borders. *shock*

--- src/app/(frontend)/home/things.tsx
+++ src/app/(frontend)/home/things.tsx
@@ -8,12 +8,12 @@ export default async function Things() {
 
 	function Arrow() {
 		return (
-			<div className="xl:ml-auto relative size-11.25 overflow-clip duration-0">
-				<ArrowRight width={45} height={45} className="absolute group-hover:opacity-0 group-hover:duration-100" />
+			<div className="xl:ml-auto relative size-7.5 sm:size-11.25 text-3xl sm:text-[2.8125rem] overflow-clip duration-0">
+				<ArrowRight width="1em" height="1em" className="absolute group-hover:opacity-0 group-hover:duration-100" />
 				<ArrowRight
-					width={45}
-					height={45}
-					className="text-neutral-950 dark:text-white stroke-current stroke-[1px] absolute -translate-x-full group-hover:translate-x-0 group-hover:delay-100 group-hover:duration-400 ease-out-quint"
+					width="1em"
+					height="1em"
+					className="text-neutral-950 dark:text-white stroke-current stroke-[1px] absolute -translate-x-full group-hover:translate-x-0 group-hover:duration-400 ease-out-quint"
 				/>
 			</div>
 		);
@@ -26,7 +26,7 @@ export default async function Things() {
 					className="group flex items-center xl:flex-col xl:items-start justify-between col-span-2 xl:col-span-1 xl:order-3 p-6 xl:p-9 hover:bg-black/5 dark:hover:bg-white/5 duration-200 hover:duration-0"
 				>
 					<div>
-					<h2>
+					<h2 className="pb-0 xl:pb-3">
 						{t("Fonts.heading")}
 						<span className="text-green">.</span>
 					</h2>
@@ -36,11 +36,11 @@ export default async function Things() {
 				</Link>
 				<Link
 					href="/fonts/varia"
-					className="hover:brightness-110 hover:contrast-90 duration-200 hover:duration-0 xl:col-span-2 xl:row-span-2 relative w-full aspect-video xl:border-r border-black/5 dark:border-white/5"
+					className="hover:brightness-110 hover:contrast-90 duration-200 hover:duration-0 xl:col-span-2 xl:row-span-2 relative w-full aspect-video overflow-hidden"
 				>
 					<FadingImage src="/api/assets/file/MNVaria_Thumbnail.png" width={1920} height={1080} alt="MN Varia" />
 				</Link>
-				<Link href="/fonts/covert" className="hover:brightness-110 hover:contrast-90 duration-200 hover:duration-0 relative w-full aspect-video">
+				<Link href="/fonts/covert" className="hover:brightness-110 hover:contrast-90 duration-200 hover:duration-0 relative w-full aspect-video overflow-hidden">
 					<FadingImage src="/api/assets/file/MNCovert_Thumbnail.webp" width={1920} height={1080} alt="MN Covert" />
 				</Link>
 			</div>
@@ -51,7 +51,7 @@ export default async function Things() {
 					className="group flex items-center xl:flex-col xl:items-start justify-between col-span-2 xl:col-span-1 p-6 xl:p-9 hover:bg-black/5 dark:hover:bg-white/5 duration-200 hover:duration-0"
 				>
 					<div>
-					<h2>
+					<h2 className="pb-0 xl:pb-3">
 						{t("Photos.heading")}
 						<span className="text-green">.</span>
 					</h2>
@@ -61,7 +61,7 @@ export default async function Things() {
 				</Link>
 				<Link
 					href="/photos"
-					className="hover:brightness-110 hover:contrast-90 duration-200 hover:duration-0 xl:col-span-2 xl:row-span-2 relative w-full aspect-3/2 xl:border-l border-black/5 dark:border-white/5 overflow-hidden"
+					className="hover:brightness-110 hover:contrast-90 duration-200 hover:duration-0 xl:col-span-2 xl:row-span-2 relative w-full aspect-3/2 overflow-hidden"
 				>
 					<FadingImage src="/api/photos/file/DSC00275.webp" width={6192} height={4128} alt="" />
 				</Link>
@@ -76,7 +76,7 @@ export default async function Things() {
 					className="group flex items-center xl:flex-col xl:items-start justify-between col-span-2 xl:col-span-1 xl:order-1 p-6 xl:p-9 hover:bg-black/5 dark:hover:bg-white/5 duration-200 hover:duration-0"
 				>
 					<div>
-					<h2>
+					<h2 className="pb-0 xl:pb-3">
 						{t("Graphics.heading")}
 						<span className="text-green">.</span>
 					</h2>
@@ -86,13 +86,13 @@ export default async function Things() {
 				</Link>
 				<Link
 					href="/graphics/solar-system-3"
-					className="hover:brightness-110 hover:contrast-90 duration-200 hover:duration-0 xl:col-span-2 xl:row-span-2 relative w-full aspect-video xl:border-r border-black/5 dark:border-white/5"
+					className="hover:brightness-110 hover:contrast-90 duration-200 hover:duration-0 xl:col-span-2 xl:row-span-2 relative w-full aspect-video overflow-hidden"
 				>
 					<FadingImage src="/api/assets/file/Solar_System_2_f133addf64.webp" width={1920} height={1080} alt="" />
 				</Link>
 				<Link
 					href="http://localhost:3000/graphics/opensuse-rebrand-concept"
-					className="hover:brightness-110 hover:contrast-90 duration-200 hover:duration-0 relative w-full aspect-video xl:order-2"
+					className="hover:brightness-110 hover:contrast-90 duration-200 hover:duration-0 relative w-full aspect-video xl:order-2 overflow-hidden"
 				>
 					<FadingImage src="/api/assets/file/openSUSE_overview_19d4d56646.png" width={1920} height={1080} alt="" />
 				</Link>
